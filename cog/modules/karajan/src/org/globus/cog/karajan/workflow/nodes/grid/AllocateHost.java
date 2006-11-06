7a3a9cd18eba5a7ed9a24df0ec4a5abc13ef54fb
Mihael Hategan <hategan@mcs.anl.gov>
2006-11-06 19:56:04 +0000

    updated allocateHost
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1344 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/grid/AllocateHost.java
+++ cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/grid/AllocateHost.java
@@ -7,7 +7,11 @@
 package org.globus.cog.karajan.workflow.nodes.grid;
 
 import org.apache.log4j.Logger;
+import org.globus.cog.abstraction.impl.common.StatusEvent;
+import org.globus.cog.abstraction.interfaces.Status;
+import org.globus.cog.abstraction.interfaces.StatusListener;
 import org.globus.cog.karajan.arguments.Arg;
+import org.globus.cog.karajan.scheduler.ContactAllocationTask;
 import org.globus.cog.karajan.scheduler.Scheduler;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.util.Contact;
@@ -15,12 +19,12 @@ import org.globus.cog.karajan.util.TypeUtil;
 import org.globus.cog.karajan.workflow.ExecutionException;
 import org.globus.cog.karajan.workflow.nodes.PartialArgumentsContainer;
 
-public class AllocateHost extends PartialArgumentsContainer {
+public class AllocateHost extends PartialArgumentsContainer implements StatusListener {
 	public static final Logger logger = Logger.getLogger(AllocateHost.class);
 
 	public static final Arg A_NAME = new Arg.Positional("name");
 	public static final Arg A_CONSTRAINTS = new Arg.Optional("constraints", null);
-	
+
 	static {
 		setArguments(AllocateHost.class, new Arg[] { A_NAME, A_CONSTRAINTS });
 	}
@@ -30,21 +34,61 @@ public class AllocateHost extends PartialArgumentsContainer {
 	}
 
 	protected void partialArgumentsEvaluated(VariableStack stack) throws ExecutionException {
-		Object constraints = A_CONSTRAINTS.getValue(stack); 
+		Object constraints = A_CONSTRAINTS.getValue(stack);
 		try {
 			Scheduler s = (Scheduler) stack.getDeepVar(SchedulerNode.SCHEDULER);
-			Contact contact = s.allocateContact(constraints);
-			logger.debug("Allocated host " + contact);
-			stack.setVar(TypeUtil.toString(A_NAME.getValue(stack)), contact);
+			if (constraints == null) {
+				Contact contact = s.allocateContact();
+				if (logger.isDebugEnabled()) {
+					logger.debug("Allocated host " + contact);
+				}
+				stack.setVar(TypeUtil.toString(A_NAME.getValue(stack)), contact);
+				super.partialArgumentsEvaluated(stack);
+				startRest(stack);
+			}
+			else {
+				ContactAllocationTask t = new ContactAllocationTask();
+				t.setStack(stack);
+				Contact contact = s.allocateContact(constraints);
+				s.addJobStatusListener(this, t);
+				s.enqueue(t, new Contact[] { contact });
+			}
 		}
 		catch (ExecutionException e) {
 			throw e;
 		}
 		catch (Exception e) {
 			fail(stack, e.getMessage());
-			return;
 		}
-		super.partialArgumentsEvaluated(stack);
-		startRest(stack);
+	}
+
+	public void statusChanged(StatusEvent event) {
+		ContactAllocationTask t = (ContactAllocationTask) event.getSource();
+		VariableStack stack = t.getStack();
+		try {
+			int code = event.getStatus().getStatusCode();
+			if (code == Status.FAILED) {
+				Exception e = event.getStatus().getException();
+				if (e == null) {
+					failImmediately(stack, "Failed to allocate host");
+				}
+				else {
+					failImmediately(stack, event.getStatus().getException());
+				}
+			}
+			else if (code == Status.COMPLETED) {
+				stack.setVar(TypeUtil.toString(A_NAME.getValue(stack)), t.getContact());
+				super.partialArgumentsEvaluated(stack);
+				startRest(stack);
+			}
+		}
+		catch (ExecutionException e) {
+			try {
+				failImmediately(stack, e);
+			}
+			catch (ExecutionException e1) {
+				logger.warn("Could not fail element", e1);
+			}
+		}
 	}
 }
