616c996d6588ee32abe94c903f811049285873af
Mihael Hategan <hategan@mcs.anl.gov>
2006-11-22 20:36:56 +0000

    added some javadocs
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1413 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/karajan/src/org/globus/cog/karajan/scheduler/ResourceConstraintChecker.java
+++ cog/modules/karajan/src/org/globus/cog/karajan/scheduler/ResourceConstraintChecker.java
@@ -13,9 +13,31 @@ import java.util.List;
 
 import org.globus.cog.karajan.util.BoundContact;
 
-//I'm terrible with naming classes
+// I'm terrible with naming classes
 public interface ResourceConstraintChecker {
+	/**
+	 * Checks if a resource meets the given task constraints
+	 * 
+	 * @param resource
+	 *            the resource to be checked
+	 * @param tc
+	 *            the constraints to check the resource against
+	 * 
+	 * @return <code>true</code> if the resouce meets the constraints;
+	 *         <code>false</code> otherwise
+	 */
 	boolean checkConstraints(BoundContact resource, TaskConstraints tc);
-	
+
+	/**
+	 * Identifies the resouces in a list meeting the given task constraints
+	 * 
+	 * @param resources
+	 *            a list of BoundContact objects
+	 * @param tc
+	 *            the task constraints
+	 * @return the subset of contacts meeting the given constraints
+	 * 
+	 * @see BoundContact
+	 */
 	List checkConstraints(List resources, TaskConstraints tc);
 }
