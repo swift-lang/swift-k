17e4bc6ee29bb24ba283789eb1dbdde7fe6ea92b
Mihael Hategan <hategan@mcs.anl.gov>
2006-11-30 22:31:21 +0000

    tweaks (fixes actually) to the wh scheduler
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@1440 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHost.java
+++ src/cog/modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHost.java
@@ -9,31 +9,74 @@
  */
 package org.globus.cog.karajan.scheduler;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import org.globus.cog.karajan.util.BoundContact;
 
 public class WeightedHost implements Comparable {
 	private BoundContact host;
 	private Double score;
+	private double tscore;
+	private int load;
+	private double delayedDelta;
 
 	public WeightedHost(BoundContact contact) {
-		this(contact, 1.0);
+		this(contact, 0.0);
 	}
 
 	public WeightedHost(BoundContact contact, double score) {
+		this(contact, score, 0);
+	}
+	
+	public WeightedHost(BoundContact contact, double score, int load) {
 		this.host = contact;
+		setScore(score);
+		this.load = load;
+	}
+
+	protected void setScore(double score) {
 		this.score = new Double(score);
+		this.tscore = smooth(score);
+	}
+
+	public static final double T = 100;
+	public static final double B = 2.0*Math.log(T)/Math.PI;
+	public static final double C = 0.2;
+
+	public double smooth(double score) {
+		return Math.exp(B*Math.atan(C*score));
 	}
 
 	public final double getScore() {
 		return score.doubleValue();
 	}
-	
+
 	public final Double getScoreAsDouble() {
 		return score;
 	}
-	
+
+	public final double getTScore() {
+		return tscore;
+	}
+
 	public final BoundContact getHost() {
 		return host;
+	}
+
+	public int getLoad() {
+		return load;
+	}
+
+	public void setLoad(int load) {
+		this.load = load;
+	}
+
+	public synchronized void changeLoad(int dl) {
+		load += dl;
+		if (load < 0) {
+			load = 0;
+		}
 	}
 
 	public boolean equals(Object obj) {
@@ -48,19 +91,36 @@ public class WeightedHost implements Comparable {
 		return host.hashCode();
 	}
 
+	public static final NumberFormat D4;
+	static {
+		D4 = DecimalFormat.getInstance();
+		D4.setMaximumFractionDigits(3);
+		D4.setMinimumFractionDigits(3);
+	}
+
 	public String toString() {
-		return host.toString();
+		return host.toString() + ":" + D4.format(score) + "(" + D4.format(tscore) + "):"+load;
 	}
 
 	public int compareTo(Object o) {
 		WeightedHost other = (WeightedHost) o;
 		int r = score.compareTo(other.score);
 		if (r == 0) {
-			//arbitrary ordering on the contact
+			// arbitrary ordering on the contact
 			return System.identityHashCode(host) - System.identityHashCode(other.host);
 		}
 		else {
 			return r;
 		}
 	}
+
+	public double getDelayedDelta() {
+		return delayedDelta;
+	}
+
+	public void setDelayedDelta(double delayedDelta) {
+		this.delayedDelta = delayedDelta;
+	}
+	
+	
 }
