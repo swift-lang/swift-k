fec97533c75a1287a2053cc8437f830a09d9569c
Mihael Hategan <hategan@mcs.anl.gov>
2006-12-12 20:17:08 +0000

    fixed remote marsaling
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1456 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/commands/RemoteElementMarshallingPolicy.java
+++ cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/commands/RemoteElementMarshallingPolicy.java
@@ -25,16 +25,23 @@ public class RemoteElementMarshallingPolicy extends AbstractElementMarshallingPo
 	public RemoteElementMarshallingPolicy(KarajanSerializationContext kcontext) {
 		super(kcontext);
 	}
-	
+
 	public void marshal(Object elem, HierarchicalStreamWriter wr, MarshallingContext context) {
 		FlowElement fe = (FlowElement) elem;
 		boolean source = getKContext().isSource();
 		if (!source) {
+			getKContext().setSource(true);
 			wr.addAttribute("_source", "true");
+			wr.addAttribute("_uid", "-1");
+			wr.startNode(fe.getElementType());
+			this.writeProperties(fe, wr, context);
+			this.writeChildren(fe, wr, context);
+			wr.endNode();
+			getKContext().setSource(false);
 		}
-		getKContext().setSource(true);
-		this.writeProperties(fe, wr, context);
-		this.writeChildren(fe, wr, context);
-		getKContext().setSource(source);
+		else {
+			this.writeProperties(fe, wr, context);
+			this.writeChildren(fe, wr, context);
+		}
 	}
 }
