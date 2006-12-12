593bd647379e906c5fa7da7a890bb207fcbfe9d0
Mihael Hategan <hategan@mcs.anl.gov>
2006-12-12 21:44:24 +0000

    fixed anchors
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@61 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- docs/buildguides.sh
+++ docs/buildguides.sh
@@ -4,5 +4,5 @@ for guide in *.xml; do
 	out=${guide:0:${#guide}-4}.shtml
 	xsltproc formatting/vdl2_html.xsl $guide
 	#that's 'cause --output doesn't work
-	mv index.html $out
+	sed -e "s/index.html#/#/" index.html >$out
 done
