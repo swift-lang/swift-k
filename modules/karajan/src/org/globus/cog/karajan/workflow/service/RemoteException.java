bec5a5b5fd22b175d6cbaac2eb05adc0c61de786
Mihael Hategan <hategan@mcs.anl.gov>
2006-12-12 20:18:04 +0000

    other service fixes
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@1458 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ modules/karajan/src/org/globus/cog/karajan/workflow/service/RemoteException.java
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Dec 8, 2006
+ */
+package org.globus.cog.karajan.workflow.service;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class RemoteException extends Exception {
+	private String remote;
+	public RemoteException(String msg, String remote) {
+		super(msg);
+		this.remote = remote;
+	}
+	
+	public void printStackTrace(PrintStream s) {
+		super.printStackTrace(s);
+		s.println("Remote exception:");
+		s.println(remote);
+	}
+	public void printStackTrace(PrintWriter s) {
+		super.printStackTrace(s);
+		s.println("Remote exception:");
+		s.println(remote);
+	}
+}
