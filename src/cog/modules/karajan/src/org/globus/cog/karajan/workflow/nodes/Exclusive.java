d3df595a53cdfcb342d46e5a52163548ea8dc6c9
Mihael Hategan <hategan@mcs.anl.gov>
2006-12-30 17:45:26 +0000

    can also synchronize on user supplied values now
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@1509 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/Exclusive.java
+++ src/cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/Exclusive.java
@@ -9,35 +9,77 @@
  */
 package org.globus.cog.karajan.workflow.nodes;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
+import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.workflow.ExecutionException;
 
-public class Exclusive extends Sequential {
-	private final LinkedList instances;
-	private boolean locked;
+public class Exclusive extends PartialArgumentsContainer {
+	public static final String LOCKS = "#exclusive:locks";
+	public static final String ON = "#on";
 
-	public Exclusive() {
-		instances = new LinkedList();
+	public static final Arg A_ON = new Arg.Optional("on");
+
+	static {
+		setArguments(Exclusive.class, new Arg[] { A_ON });
 	}
 
-	protected synchronized void pre(VariableStack stack) throws ExecutionException {
+	protected void partialArgumentsEvaluated(VariableStack stack) throws ExecutionException {
 		super.pre(stack);
-		if (locked) {
-			instances.addFirst(stack);
-		}
-		else {
-			locked = true;
-			this.executeChildren(stack);
+		Map locks = getLocks(stack);
+		Object on = A_ON.getValue(stack, Object.class);
+		stack.setVar(ON, on);
+		if (testAndAdd(locks, on, stack)) {
+			startRest(stack);
 		}
 	}
 
-	protected synchronized void _finally(VariableStack stack) throws ExecutionException {
-		locked = false;
-		if (instances.size() > 0) {
-			VariableStack other = (VariableStack) instances.removeLast();
-			pre(other);
+	protected boolean testAndAdd(Map locks, Object on, VariableStack stack) {
+		synchronized (locks) {
+			LinkedList stacks = (LinkedList) locks.get(on);
+			boolean first;
+			if (stacks == null) {
+				first = true;
+				stacks = new LinkedList();
+				locks.put(on, stacks);
+			}
+			else {
+				first = false;
+				stacks.add(stack);
+			}
+			return first;
+		}
+	}
+
+	private Map getLocks(VariableStack stack) {
+		synchronized (stack.getExecutionContext()) {
+			Map locks = (Map) stack.getGlobal(LOCKS);
+			if (locks == null) {
+				locks = new HashMap();
+				stack.setGlobal(LOCKS, locks);
+			}
+			return locks;
+		}
+	}
+
+	protected void _finally(VariableStack stack) throws ExecutionException {
+		Map locks = getLocks(stack);
+		VariableStack next = null;
+		synchronized (locks) {
+			Object on = stack.currentFrame().getVar(ON);
+			LinkedList instances = (LinkedList) locks.get(on);
+			if (instances.size() > 0) {
+				next = (VariableStack) instances.removeFirst();
+			}
+			else {
+				locks.remove(on);
+			}
+		}
+		if (next != null) {
+			pre(next);
 		}
 		super._finally(stack);
 	}
