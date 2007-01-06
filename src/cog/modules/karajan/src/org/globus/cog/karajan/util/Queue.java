7872b1d2c148a36028b03df738e98645e074eb85
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-06 20:50:11 +0000

    better queue for the scheduler
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@1529 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/karajan/src/org/globus/cog/karajan/util/Queue.java
+++ src/cog/modules/karajan/src/org/globus/cog/karajan/util/Queue.java
@@ -1,4 +1,3 @@
-
 // ----------------------------------------------------------------------
 // This code is developed as part of the Java CoG Kit project
 // The terms of the license can be found at http://www.cogkit.org/license
@@ -7,56 +6,114 @@
 
 package org.globus.cog.karajan.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-
 public final class Queue {
-	private final List list;
+	private Entry head;
 	private int size;
 
 	public Queue() {
-		list = new ArrayList();
+		head = new Entry(null, null, null);
+		head.prev = head;
+		head.next = head;
 		size = 0;
 	}
 
-	public void enqueue(Object o) {
-		list.add(o);
+	public synchronized void enqueue(Object o) {
+		Entry e = new Entry(o, head.prev, head);
+		head.prev.next = e;
+		head.prev = e;
 		size++;
 	}
-
-	public Object dequeue() {
-		size--;
-		return list.remove(0);
+	
+	public synchronized Object dequeue() { 
+		Object o = head.next.obj;
+		head.next.next.prev = head;
+		head.next = head.next.next;
+		return o;
 	}
 
-	public Object peek(){
-		if (size >= 1){
-			return list.get(0);
-		}
-		else{
-			return null;
-		}
-	}
 	public boolean isEmpty() {
-		return size==0;
+		return size == 0;
 	}
 
 	public int size() {
 		return size;
 	}
-	
-	public Object remove(int index){
-		size--;
-	    return list.remove(index);
+
+	public Cursor cursor() {
+		return new C();
 	}
-	
-	public Object get(int index){
-	    return list.get(index);
+
+	private class Entry {
+		private final Object obj;
+		private Entry next, prev;
+
+		public Entry(Object obj, Entry prev, Entry next) {
+			this.obj = obj;
+			this.next = next;
+			this.prev = prev;
+		}
+
+		public String toString() {
+			return "Entry(" + obj + ")";
+		}
 	}
-	
-	public Collection getAll() {
-		return list;
+
+	public interface Cursor {
+		boolean hasNext();
+
+		void remove();
+
+		Object next();
+
+		void reset();
+	}
+
+	private class C implements Cursor {
+		private Entry crt;
+
+		public C() {
+			reset();
+		}
+
+		public boolean hasNext() {
+			return crt.next != head;
+		}
+
+		public synchronized Object next() {
+			crt = crt.next;
+			return crt.obj;
+		}
+
+		public synchronized void remove() {
+			remove(crt);
+		}
+
+		private void remove(Entry e) {
+			size--;
+			e.next.prev = e.prev;
+			e.prev.next = e.next;
+		}
+
+		public void reset() {
+			crt = head;
+		}
+	}
+
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		q.enqueue("a");
+		q.enqueue("b");
+		q.enqueue("c");
+		Cursor c = q.cursor();
+		System.err.println("" + c.next() + c.next() + c.next());
+		c.reset();
+		c.next();
+		c.next();
+		c.remove();
+		System.err.println("" + c.next());
+		c.reset();
+		System.err.println("" + c.next() + c.next());
+		c.reset();
+		System.err.println("" + c.next() + c.next() + c.next());
 	}
 }
