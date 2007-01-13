aaf5e22eadd5988015f5b189c230938f637c7172
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-13 00:54:31 +0000

    get/setParam are needed if parameter values consist of VDL2 data
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@198 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/mapping/Mapper.java
+++ src/org/griphyn/vdl/mapping/Mapper.java
@@ -18,7 +18,21 @@ public interface Mapper {
     /**
      * Returns true if data mapped by this mapper cannot
      * change in structure at run-time. Typically this applies
-     * to all mappers which work on pre determined data. 
+     * to all mappers which work on pre determined data.
+     */
+    
+    /* A short explanation of why this is here:
+     *   Since we don't yet deal with apps returning things with
+     *   undetermined sizes, yet I2U2 needs to return arrays, this
+     *   tells the system that it can close top-level arrays. This
+     *   allows the system to know exactly what files need to be 
+     *   staged out before a job is run if the return type of the
+     *   app is an array. This is not a permanent or nice solution,
+     *   but a necessary compromise.
      */
     boolean isStatic();
+    
+    void setParam(String name, Object value);
+    
+    Object getParam(String name);
 }
