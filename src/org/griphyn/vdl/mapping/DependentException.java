156b1c82fc1a63498727c35df5797e7315dded7b
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-13 21:37:46 +0000

    mapping deps
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@224 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/mapping/DependentException.java
+++ src/org/griphyn/vdl/mapping/DependentException.java
@@ -3,29 +3,29 @@
  */
 package org.griphyn.vdl.mapping;
 
-public class DependentException extends RuntimeException {
+/**
+ * Signifies a problem with data/mapping dependencies
+ */
+public abstract class DependentException extends RuntimeException {
 	private DSHandle handle;
-	
-	public DependentException(DSHandle handle, String message, Exception prev) {
-		super(message, prev);
-		this.handle = handle;
-	}
-	
+
 	public DependentException(DSHandle handle, Exception prev) {
 		super(prev);
 		this.handle = handle;
 	}
-	
-	public DependentException(DSHandle handle, String message) {
-		super(message);
+
+	public DependentException(DSHandle handle) {
+		super();
 		this.handle = handle;
 	}
+	
+	public abstract String getMessage();
 
 	public DSHandle getHandle() {
 		return handle;
 	}
 
 	public String toString() {
-		return super.getMessage();
+		return getMessage();
 	}
 }
