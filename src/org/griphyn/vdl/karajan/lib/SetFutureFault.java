c6f89f2fdc42a4eb17327f15b5ff700e33c3a4f1
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-13 00:47:56 +0000

    same
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@195 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/SetFutureFault.java
+++ src/org/griphyn/vdl/karajan/lib/SetFutureFault.java
@@ -3,14 +3,18 @@
  */
 package org.griphyn.vdl.karajan.lib;
 
+import org.apache.log4j.Logger;
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.util.TypeUtil;
 import org.globus.cog.karajan.workflow.ExecutionException;
 import org.griphyn.vdl.mapping.DSHandle;
+import org.griphyn.vdl.mapping.DependentException;
 import org.griphyn.vdl.mapping.Path;
 
 public class SetFutureFault extends VDLFunction {
+	public static final Logger logger = Logger.getLogger(SetFutureFault.class);
+
 	public static final Arg PA_VALUE = new Arg.Positional("value");
 
 	static {
@@ -22,21 +26,23 @@ public class SetFutureFault extends VDLFunction {
 		try {
 			Path path = parsePath(OA_PATH.getValue(stack), stack);
 			DSHandle leaf = var.getField(path);
+			if (logger.isInfoEnabled()) {
+				logger.info("Failing " + leaf);
+			}
 			synchronized (leaf) {
 				Object value = PA_VALUE.getValue(stack);
-				if (value instanceof ExecutionException) {
-					leaf.setValue(value);
+				if (value instanceof Exception) {
+					leaf.setValue(new DependentException(leaf, (Exception) value));
 				}
 				else {
-					leaf.setValue(new ExecutionException(TypeUtil.toString(value)));
+					leaf.setValue(new DependentException(leaf, TypeUtil.toString(value)));
 				}
 				closeShallow(stack, leaf);
 			}
-			return null;
 		}
 		catch (Exception e) {
-			throw new ExecutionException(e.getMessage() + " for variable " + var, e);
+			throw new ExecutionException(e);
 		}
+		return null;
 	}
-
 }
