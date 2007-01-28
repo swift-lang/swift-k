40ea99cce7b8d0ca3ddfa46f61fda28367de50f4
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-28 17:45:05 +0000

    updated exception model
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@1554 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ src/cog/modules/provider-gt2/src/org/globus/cog/abstraction/impl/file/ftp/AbstractFTPFileResource.java
@@ -0,0 +1,59 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Jan 27, 2007
+ */
+package org.globus.cog.abstraction.impl.file.ftp;
+
+import java.io.IOException;
+
+import org.globus.cog.abstraction.impl.file.AbstractFileResource;
+import org.globus.cog.abstraction.impl.file.FileResourceException;
+import org.globus.cog.abstraction.impl.file.IrrecoverableResourceException;
+import org.globus.cog.abstraction.interfaces.SecurityContext;
+import org.globus.cog.abstraction.interfaces.ServiceContact;
+import org.globus.ftp.exception.FTPException;
+import org.globus.ftp.exception.ServerException;
+
+public abstract class AbstractFTPFileResource extends AbstractFileResource {
+
+    public AbstractFTPFileResource() {
+        super();   
+    }
+
+    public AbstractFTPFileResource(String name, String protocol, ServiceContact serviceContact, SecurityContext securityContext) {
+        super(name, protocol, serviceContact, securityContext);        
+    }
+
+    protected FileResourceException translateException(Exception e) {
+        return translateException(null, e);
+    }
+
+    protected FileResourceException translateException(String message, Exception e) {
+        if (e instanceof IOException) {
+            return new IrrecoverableResourceException(message, e);
+        }
+        else if (e instanceof FTPException) {
+            if (e instanceof ServerException) {
+                ServerException se = (ServerException) e;
+                if (se.getCode() == ServerException.REPLY_TIMEOUT) {
+                    return new IrrecoverableResourceException(message, e);
+                }
+                else {
+                    return new FileResourceException(message, e);
+                }
+            }
+            else {
+                return new FileResourceException(message, e);
+            }
+        }
+        else {
+            return new IrrecoverableResourceException(message, e);
+        }
+    }
+
+}
