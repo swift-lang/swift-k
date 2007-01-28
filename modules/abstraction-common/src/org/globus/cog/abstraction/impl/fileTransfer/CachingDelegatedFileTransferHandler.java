161ca87f3b763ac9e2a81d152692b4b16ab756bb
Mihael Hategan <hategan@mcs.anl.gov>
2007-01-28 17:34:15 +0000

    deal better with irrecoverable errors
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@1552 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/abstraction-common/src/org/globus/cog/abstraction/impl/fileTransfer/CachingDelegatedFileTransferHandler.java
+++ modules/abstraction-common/src/org/globus/cog/abstraction/impl/fileTransfer/CachingDelegatedFileTransferHandler.java
@@ -6,6 +6,7 @@
 
 package org.globus.cog.abstraction.impl.fileTransfer;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.globus.cog.abstraction.impl.common.ProviderMethodException;
@@ -13,25 +14,55 @@ import org.globus.cog.abstraction.impl.common.task.InvalidProviderException;
 import org.globus.cog.abstraction.impl.common.task.InvalidSecurityContextException;
 import org.globus.cog.abstraction.impl.file.FileResourceCache;
 import org.globus.cog.abstraction.impl.file.FileResourceException;
+import org.globus.cog.abstraction.impl.file.IrrecoverableResourceException;
 import org.globus.cog.abstraction.interfaces.FileResource;
 import org.globus.cog.abstraction.interfaces.Service;
 
-public class CachingDelegatedFileTransferHandler extends DelegatedFileTransferHandler {
+public class CachingDelegatedFileTransferHandler extends
+        DelegatedFileTransferHandler {
 
-	protected FileResource startResource(Service service) throws InvalidProviderException,
-			ProviderMethodException, InvalidSecurityContextException, FileResourceException,
-			IOException {
-		return FileResourceCache.getDefault().getResource(service);
-	}
+    protected synchronized FileResource startResource(Service service)
+            throws InvalidProviderException, ProviderMethodException,
+            InvalidSecurityContextException, FileResourceException, IOException {
+        return FileResourceCache.getDefault().getResource(service);
+    }
 
-	protected void stopResources() {
-		if (getSourceResource() != null) {
-			FileResourceCache.getDefault().releaseResource(getSourceResource());
-			setSourceResource(null);
-		}
-		if (getDestinationResource() != null) {
-			FileResourceCache.getDefault().releaseResource(getDestinationResource());
-			setDestinationResource(null);
-		}
-	}
+    protected synchronized void stopResources() {
+        if (getSourceResource() != null) {
+            FileResourceCache.getDefault().releaseResource(getSourceResource());
+            setSourceResource(null);
+        }
+        if (getDestinationResource() != null) {
+            FileResourceCache.getDefault().releaseResource(
+                    getDestinationResource());
+            setDestinationResource(null);
+        }
+    }
+
+    protected void doDestination(File localSource, Service service)
+            throws FileResourceException, IOException,
+            InvalidProviderException, ProviderMethodException,
+            InvalidSecurityContextException {
+        try {
+            super.doDestination(localSource, service);
+        }
+        catch (IrrecoverableResourceException e) {
+            FileResourceCache.getDefault().invalidateResource(
+                    getDestinationResource());
+            throw e;
+        }
+    }
+
+    protected File doSource(Service service, File localDestination)
+            throws FileResourceException, IOException,
+            InvalidProviderException, ProviderMethodException {
+        try {
+            return super.doSource(service, localDestination);
+        }
+        catch (IrrecoverableResourceException e) {
+            FileResourceCache.getDefault().invalidateResource(
+                    getSourceResource());
+            throw e;
+        }
+    }
 }
