4fb03755ad1ac7d547866875ccc78cf890ba18a2
Mihael Hategan <hategan@mcs.anl.gov>
2007-02-06 19:28:47 +0000

    better accounting of callback handlers
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1572 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ cog/modules/provider-gt2/src/org/globus/cog/abstraction/impl/execution/gt2/CallbackHandlerManager.java
@@ -0,0 +1,43 @@
+// ----------------------------------------------------------------------
+// This code is developed as part of the Java CoG Kit project
+// The terms of the license can be found at http://www.cogkit.org/license
+// This message may not be removed or altered.
+// ----------------------------------------------------------------------
+
+package org.globus.cog.abstraction.impl.execution.gt2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.globus.common.CoGProperties;
+import org.globus.gram.Gram;
+import org.ietf.jgss.GSSCredential;
+
+public class CallbackHandlerManager {
+    private static Map count = new HashMap();
+    private static String cogIP = CoGProperties.getDefault().getIPAddress();
+
+    public static synchronized void increaseUsageCount(GSSCredential cred) {
+        Integer i = (Integer) count.get(cred);
+        if (i == null) {
+            i = new Integer(1);
+        } else {
+            i = new Integer(i.intValue() + 1);
+        }
+        count.put(cred, i);
+    }
+
+    public static synchronized void decreaseUsageCount(GSSCredential cred) {
+        Integer i = (Integer) count.get(cred);
+        if (i == null) {
+            throw new IllegalStateException("No registered callback handler for "
+                    + cred );
+        } else if (i.intValue() == 1) {
+            count.remove(cred);
+            Gram.deactivateCallbackHandler(cred);
+            System.err.println(count);
+        } else {
+            count.put(cred, new Integer(i.intValue() - 1));
+        }
+    }
+}
