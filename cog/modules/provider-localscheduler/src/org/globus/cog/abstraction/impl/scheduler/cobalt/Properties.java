d4077db22d65d9614704c0d16b1387a584e05deb
Mihael Hategan <hategan@mcs.anl.gov>
2007-02-12 23:55:03 +0000

    added providers for PBS and Cobalt
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1575 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ cog/modules/provider-localscheduler/src/org/globus/cog/abstraction/impl/scheduler/cobalt/Properties.java
@@ -0,0 +1,80 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Oct 20, 2005
+ */
+package org.globus.cog.abstraction.impl.scheduler.cobalt;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+public class Properties extends java.util.Properties {
+	private static Logger logger = Logger.getLogger(Properties.class);
+
+	public static final String PROPERTIES = "provider-cobalt.properties";
+	
+	public static final String POLL_INTERVAL = "poll.interval";
+	public static final String CQSUB = "cqsub";
+	public static final String CQSTAT = "cqstat";	
+
+	private static Properties properties;
+
+	public static synchronized Properties getProperties() {
+		if (properties == null) {
+			properties = new Properties();
+			properties.load();
+		}
+		return properties;
+	}
+
+	private void load() {
+		setDefaults();
+		InputStream is = getClass().getClassLoader().getResourceAsStream(PROPERTIES);
+		if (is == null) {
+			logger.warn("Could not find " + PROPERTIES + ". Using defaults.");
+		}
+		else {
+			try {
+				super.load(is);
+			}
+			catch (IOException e) {
+			}
+		}
+	}
+
+	private void setDefaults() {
+		setPollInterval(5);
+		setCQSub("cqsub");
+		setCQStat("cqstat");
+	}
+
+	public void setPollInterval(int value) {
+	    setProperty(POLL_INTERVAL, String.valueOf(value));
+	}
+	
+	public int getPollInterval() {
+	    return Integer.parseInt(getProperty(POLL_INTERVAL));
+	}
+	
+	public void setCQSub(String cqsub) {
+	    setProperty(CQSUB, cqsub);
+	}
+	
+	public String getCQSub() {
+	    return getProperty(CQSUB);
+	}
+	
+	public void setCQStat(String cqstat) {
+	    setProperty(CQSTAT, cqstat);
+	}
+	
+	public String getCQStat() {
+	    return getProperty(CQSTAT);
+	}
+}
