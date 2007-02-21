e0d3b97137b6f14b79885c4faf6ce3975c854b17
Mihael Hategan <hategan@mcs.anl.gov>
2007-02-21 19:56:25 +0000

    renamed
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@456 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ src/org/griphyn/vdl/util/SwiftProfile.java
@@ -0,0 +1,8 @@
+/*
+ * Created on Dec 29, 2006
+ */
+package org.griphyn.vdl.util;
+
+public interface SwiftProfile {
+	public static final String KEY_STORAGE_SIZE = "swift::storagesize";
+}
--- src/org/griphyn/vdl/util/VDL2Profile.java
+++
@@ -1,8 +0,0 @@
-/*
- * Created on Dec 29, 2006
- */
-package org.griphyn.vdl.util;
-
-public interface VDL2Profile {
-	public static final String KEY_STORAGE_SIZE = "VDL2::storagesize";
-}
