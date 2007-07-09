4418a14000dfbdae9dbb38d9c155cc6578390437
Ben Clifford <benc@hawaga.org.uk>
2007-07-09 11:42:33 +0000

    language tests of boolean operators used 'int' when they should have used 'boolean'. this doesn't break the present implementation, but does in some still-in-development changes that do different type checking.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@897 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/language-behaviour/018-less.swift
+++ tests/language-behaviour/018-less.swift
@@ -1,6 +1,6 @@
 type messagefile {}
 
-(messagefile t) greeting(int m) { 
+(messagefile t) greeting(boolean m) { 
     app {
         echo m stdout=@filename(t);
     }
@@ -8,7 +8,7 @@ type messagefile {}
 
 messagefile outfile <"018-less.out">;
 
-int i = 7<9;
+boolean i = 7<9;
 
 outfile = greeting(i);
 
