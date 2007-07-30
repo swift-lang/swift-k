779c0c9a76dcd06701a4f773f47555f86d1b5455
Mihael Hategan <hategan@mcs.anl.gov>
2007-07-30 18:42:52 +0000

    organized imports; removed cog banner
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1015 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/Misc.java
+++ src/org/griphyn/vdl/karajan/lib/Misc.java
@@ -1,46 +1,13 @@
-// ----------------------------------------------------------------------
-// This code is developed as part of the Java CoG Kit project
-// The terms of the license can be found at http://www.cogkit.org/license
-// This message may not be removed or altered.
-// ----------------------------------------------------------------------
 package org.griphyn.vdl.karajan.lib;
 
-// TODO lose these imports
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.RandomAccessFile;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.globus.cog.karajan.arguments.Arg;
-import org.globus.cog.karajan.arguments.VariableArguments;
 import org.globus.cog.karajan.stack.VariableStack;
-import org.globus.cog.karajan.util.Identifier;
-import org.globus.cog.karajan.util.KarajanIterator;
-import org.globus.cog.karajan.util.Property;
-import org.globus.cog.karajan.util.RangeIterator;
 import org.globus.cog.karajan.util.TypeUtil;
 import org.globus.cog.karajan.workflow.ExecutionException;
-import org.globus.cog.karajan.workflow.futures.FutureVariableArguments;
-
 import org.griphyn.vdl.mapping.DSHandle;
 import org.griphyn.vdl.mapping.RootDataNode;
 
@@ -86,7 +53,7 @@ public class Misc extends FunctionsCollection {
 		String group;
 		try {
 			Pattern p = Pattern.compile(pattern);
-// TODO probably should memoize this?
+			// TODO probably should memoize this?
 
 			Matcher m = p.matcher(inputString);
 			m.find();
