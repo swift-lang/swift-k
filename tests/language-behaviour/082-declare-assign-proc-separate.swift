68705c792fbd25038d9fae915deb5b0ca1d800b5
Ben Clifford <benc@hawaga.org.uk>
2007-08-05 14:39:09 +0000

    test that output file mapping with no specified mapper works
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1058 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/language-behaviour/082-declare-assign-proc-separate.swift
@@ -0,0 +1,13 @@
+type messagefile {}
+
+(messagefile t) greeting() { 
+    app {
+        echo "hello" stdout=@filename(t);
+    }
+}
+
+messagefile outfile;
+outfile = greeting();
+
+// can't check the output in present framework because don't know
+// what filename got chosen for outfile...
