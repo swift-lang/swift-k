30dce15436668954e8752b0c99537680a130448d
Mihael Hategan <hategan@mcs.anl.gov>
2007-08-24 02:22:56 +0000

    work in progress; eventually the log should store all file activity
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1098 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/LogVar.java
+++ src/org/griphyn/vdl/karajan/lib/LogVar.java
@@ -9,12 +9,17 @@ import org.globus.cog.karajan.workflow.ExecutionException;
 import org.globus.cog.karajan.workflow.nodes.restartLog.RestartLog;
 
 public class LogVar extends VDLFunction {
+	public static final Arg PA_HOST = new Arg.Positional("host");
+	public static final Arg PA_DIR = new Arg.Positional("dir");
+	public static final Arg PA_NAME = new Arg.Positional("name");
+
 	static {
-		setArguments(LogVar.class, new Arg[] { PA_VAR, PA_PATH });
+		setArguments(LogVar.class, new Arg[] { PA_VAR, PA_PATH, PA_HOST, PA_DIR, PA_NAME });
 	}
 
 	public Object function(VariableStack stack) throws ExecutionException {
-		RestartLog.LOG_CHANNEL.ret(stack, getFileName(stack));
+		RestartLog.LOG_CHANNEL.ret(stack, getFileName(stack) + "/" + PA_HOST.getValue(stack) + "/"
+				+ PA_DIR.getValue(stack) + "/" + PA_NAME.getValue(stack));
 		return null;
 	}
 }
