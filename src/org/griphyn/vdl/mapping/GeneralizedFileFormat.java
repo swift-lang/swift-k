9bccdbc7a70bc7dbe99954961d1851d4b9a5c5f5
Mihael Hategan <hategan@mcs.anl.gov>
2007-08-24 02:31:33 +0000

    added a physical format
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1103 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ src/org/griphyn/vdl/mapping/GeneralizedFileFormat.java
@@ -0,0 +1,8 @@
+/*
+ * Created on Aug 12, 2007
+ */
+package org.griphyn.vdl.mapping;
+
+public interface GeneralizedFileFormat extends PhysicalFormat {
+	String getPath();
+}
---
+++ src/org/griphyn/vdl/mapping/PhysicalFormat.java
@@ -0,0 +1,8 @@
+/*
+ * Created on Aug 12, 2007
+ */
+package org.griphyn.vdl.mapping;
+
+public interface PhysicalFormat {
+	String getType();
+}
