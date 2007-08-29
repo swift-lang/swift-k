99af828784651682759664f67eac8dab86bff761
Mihael Hategan <hategan@mcs.anl.gov>
2007-08-29 20:30:17 +0000

    simplified things a little
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1136 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/swiftscript/FileName.java
+++ src/org/griphyn/vdl/karajan/lib/swiftscript/FileName.java
@@ -4,9 +4,7 @@ import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.workflow.ExecutionException;
 import org.griphyn.vdl.karajan.lib.VDLFunction;
-import org.griphyn.vdl.mapping.DSHandle;
 import org.griphyn.vdl.mapping.RootDataNode;
-import org.griphyn.vdl.type.NoSuchTypeException;
 import org.griphyn.vdl.type.Types;
 
 public class FileName extends VDLFunction {
@@ -16,18 +14,6 @@ public class FileName extends VDLFunction {
 
 	public Object function(VariableStack stack) throws ExecutionException {
 		String s = argList(filename(stack), true);
-		DSHandle handle;
-
-		try {
-			handle = new RootDataNode(Types.STRING);
-		}
-		catch (NoSuchTypeException nste) {
-			throw new RuntimeException(nste);
-		}
-
-		handle.setValue(s);
-
-		handle.closeShallow();
-		return handle;
+		return RootDataNode.newNode(Types.STRING, s);
 	}
 }
