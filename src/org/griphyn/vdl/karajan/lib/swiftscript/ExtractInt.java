ed82f0a3936b6c603a18d22fe980a16b3f72b4dc
Mihael Hategan <hategan@mcs.anl.gov>
2007-10-09 01:14:46 +0000

    simplified implementation a bit
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1336 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/swiftscript/ExtractInt.java
+++ src/org/griphyn/vdl/karajan/lib/swiftscript/ExtractInt.java
@@ -1,50 +1,38 @@
 package org.griphyn.vdl.karajan.lib.swiftscript;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.workflow.ExecutionException;
-import org.globus.cog.karajan.workflow.futures.FutureNotYetAvailable;
 import org.griphyn.vdl.karajan.lib.VDLFunction;
 import org.griphyn.vdl.mapping.DSHandle;
 import org.griphyn.vdl.mapping.HandleOpenException;
 import org.griphyn.vdl.mapping.RootDataNode;
 import org.griphyn.vdl.type.Types;
 
-import java.io.*;
-
 public class ExtractInt extends VDLFunction {
 	static {
 		setArguments(ExtractInt.class, new Arg[] { PA_VAR });
 	}
 
-	public Object function(VariableStack stack) throws ExecutionException {
+	public Object function(VariableStack stack) throws ExecutionException, HandleOpenException {
 		DSHandle handle = null;
 		try {
 			handle = (DSHandle) PA_VAR.getValue(stack);
-
-			if (!handle.isClosed()) {
-
-				if (logger.isDebugEnabled()) {
-					logger.debug("Waiting for " + handle);
-				}
-				throw new FutureNotYetAvailable(VDLFunction.addFutureListener(stack, handle));
-			}
-
 			String fn = argList(filename(handle), true);
 			Reader freader = new FileReader(fn);
 			BufferedReader breader = new BufferedReader(freader);
 			String str = breader.readLine();
 			freader.close();
 			Double i = new Double(str);
-
 			return RootDataNode.newNode(Types.FLOAT, i);
-		} catch(IOException ioe) {
-			throw new ExecutionException("reading integer content of file",ioe);
-		} catch(HandleOpenException he) {
-throw new FutureNotYetAvailable(VDLFunction.addFutureListener(stack, handle));
-
+		}
+		catch (IOException ioe) {
+			throw new ExecutionException("Reading integer content of file", ioe);
 		}
 	}
 }
