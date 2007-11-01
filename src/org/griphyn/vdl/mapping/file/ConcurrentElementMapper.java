eac3e8d36ec361e96469621fd08d9f6273123407
Ben Clifford <benc@hawaga.org.uk>
2007-11-01 16:26:58 +0000

    Concurrent mapper now generates filenames in a relatively deep directory hierarchy in order to reduce the number of files in each directory. There is a (hard-coded) parameter which determines how many files and subdirectories will be permitted at each level. This parameter is set to 25.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1437 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ src/org/griphyn/vdl/mapping/file/ConcurrentElementMapper.java
@@ -0,0 +1,59 @@
+package org.griphyn.vdl.mapping.file;
+
+
+/** A filename element mapper that maps paths into a fairly deep
+    directory hierarchy, with the intention that there will be
+    few elements at each level of the hierarchy. This code is
+    intended to be used in naming 'anonymous' files and so there
+    is no requirement that the way it lays files out remains
+    the same from version to version.
+  */
+
+public class ConcurrentElementMapper implements FileNameElementMapper {
+
+	/** determines how many directories and element files are permitted
+	    in each directory. There will be no more than
+	    DIRECTORY_LOAD_FACTOR element files and no more than
+	    DIRECTORY_LOAD_FACTOR directories, so there could be up to
+	    2 * DIRECTORY_LOAD_FACTOR elements. */
+	public final static int DIRECTORY_LOAD_FACTOR=25;
+
+	public String mapField(String fieldName) {
+		return "-field/"+fieldName;
+	}
+
+	public String rmapField(String pathElement) {
+		return pathElement;
+	}
+
+	public String mapIndex(int index) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("-array/");
+		sb.append(splitIndexByLoadFactor(index));
+		sb.append("/");
+		String num = String.valueOf(index);
+		sb.append("elt-"+String.valueOf(index));
+		return sb.toString();
+	}
+
+
+	String splitIndexByLoadFactor(int index)
+	{
+		if(index<=DIRECTORY_LOAD_FACTOR) {
+			return "";
+		} else {
+			String prefix = "h"+String.valueOf(index % DIRECTORY_LOAD_FACTOR) +"/";
+			String suffix = splitIndexByLoadFactor(index/DIRECTORY_LOAD_FACTOR);
+			return prefix + suffix;
+		}
+	}
+
+
+	public int rmapIndex(String pathElement) {
+		return Integer.parseInt(pathElement);
+	}
+
+	public String getSeparator(int depth) {
+		return "_";
+	}
+}
--- src/org/griphyn/vdl/mapping/file/ConcurrentMapper.java
+++ src/org/griphyn/vdl/mapping/file/ConcurrentMapper.java
@@ -7,13 +7,18 @@ import java.util.Map;
 
 import org.griphyn.vdl.mapping.MappingParam;
 
-public class ConcurrentMapper extends SimpleFileMapper {
+public class ConcurrentMapper extends AbstractFileMapper {
 	public static final MappingParam PARAM_THREAD_PREFIX = new MappingParam("thread_prefix", "");
+
+	public ConcurrentMapper() {
+		super(new ConcurrentElementMapper());
+	}
 
 	public void setParams(Map params) {
 		String prefix = (String) PARAM_PREFIX.getValue(params);
-		prefix = prefix + '-' + PARAM_THREAD_PREFIX.getValue(params);
+		prefix = "_concurrent/" + prefix + "-" + PARAM_THREAD_PREFIX.getValue(params);
 		PARAM_PREFIX.setValue(params, prefix);
 		super.setParams(params);
 	}
 }
+
