646e0ba9b70935a8c527894f9dc3e30ebc08c6fc
Ben Clifford <benc@hawaga.org.uk>
2008-01-07 20:56:22 +0000

    Expose the runID through a static method of Loader.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1555 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/Loader.java
+++ src/org/griphyn/vdl/karajan/Loader.java
@@ -61,6 +61,8 @@ public class Loader extends org.globus.cog.karajan.Loader {
 	public static final String VDL_OPERATION_TYPECHECK = "typecheck";
 	public static final String VDL_OPERATION_DRYRUN = "dryrun";
 
+	private static String runID = null;
+
 	public static void main(String[] argv) {
 		logger.debug("Loader started");
 		ArgumentParser ap = buildArgumentParser();
@@ -97,7 +99,6 @@ public class Loader extends org.globus.cog.karajan.Loader {
 		boolean runerror = false;
 		String projectName = projectName(project);
 
-		String runID;
 		if(ap.isPresent(ARG_RUNID)) {
 			runID = ap.getStringValue(ARG_RUNID);
 		} else {
@@ -411,6 +412,10 @@ public class Loader extends org.globus.cog.karajan.Loader {
 			val = val / base;
 		}
 		return sb.toString();
+	}
+
+	public static String getRunID() {
+		return runID;
 	}
 
 }
