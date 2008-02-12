7b6ed9e6af5c2561953b62d0fd2a382f3584369d
Mihael Hategan <hategan@mcs.anl.gov>
2008-02-12 17:09:13 +0000

    ensure proper ordering of events, and that all events are reported (even if by guessing)
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@1884 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ src/cog/modules/abstraction-common/src/org/globus/cog/abstraction/impl/common/task/StatusOrder.java
@@ -0,0 +1,53 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Feb 11, 2008
+ */
+package org.globus.cog.abstraction.impl.common.task;
+
+import org.globus.cog.abstraction.interfaces.Status;
+
+public class StatusOrder {
+    
+    /**
+     * Returns the predecessor of a status code or -1 if the code
+     * does not have one
+     */    
+    public static int pred(int code) {
+        switch (code) {
+            case Status.CANCELED:
+            case Status.FAILED:
+            case Status.COMPLETED:
+                return Status.ACTIVE;
+            case Status.SUBMITTING:
+                return Status.UNSUBMITTED;
+            case Status.SUBMITTED:
+                return Status.SUBMITTING;
+            case Status.ACTIVE:
+                return Status.SUBMITTED;
+            case Status.RESUMED:
+                return Status.SUSPENDED;
+            default:
+                return -1;
+        }
+    }
+    
+    /**
+     * Returns true code1 is greater than code2 in the status
+     * order. The two need not be comparable (in which case false 
+     * will be returned).
+     */
+    public static boolean greaterThan(int code1, int code2) {
+        int prev = pred(code1);
+        if (prev == -1) {
+            return false;
+        }
+        else {
+            return prev == code2 || greaterThan(prev, code2);
+        }
+    }
+}
