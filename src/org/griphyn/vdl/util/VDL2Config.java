addf9d99b52a1b913fcb1149f5e3c5edb7bbda9a
Ben Clifford <benc@hawaga.org.uk>
2008-02-25 00:27:25 +0000

    remove use of .vdl2 directory; instead emit a warning message if its detected
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1685 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/util/VDL2Config.java
+++ src/org/griphyn/vdl/util/VDL2Config.java
@@ -18,9 +18,6 @@ public class VDL2Config extends Properties {
 	public static final String[] CONFIG_FILE_SEARCH_PATH = new String[] {
 			System.getProperty("vds.home") + File.separator + "etc" + File.separator
 					+ CONFIG_FILE_NAME,
-			// included only for backwards compatibility
-			System.getProperty("user.home") + File.separator + ".vdl2" + File.separator
-					+ "vdl2.properties",
 			System.getProperty("user.home") + File.separator + ".swift" + File.separator
 					+ CONFIG_FILE_NAME };
 
@@ -32,6 +29,7 @@ public class VDL2Config extends Properties {
 	}
 
 	private static synchronized VDL2Config getDefaultConfig() throws IOException {
+		checkDeprecatedConfigFile();
 		if (config == null) {
 			config = new VDL2Config();
 			for (int i = 0; i < CONFIG_FILE_SEARCH_PATH.length; i++) {
@@ -191,4 +189,14 @@ public class VDL2Config extends Properties {
 		conf.putAll(this);
 		return conf;
 	}
+
+	// TODO this can be removed after 0.5 is released
+	static public void checkDeprecatedConfigFile() {
+		String fn = System.getProperty("user.home") + File.separator + ".vdl2" + File.separator + "vdl2.properties"; 
+		File f = new File(fn);
+		if(f.exists()) {
+			System.err.println("The .vdl2 directory is deprecated. Swift has detected the presence of a now-unsupported configuration file, "+fn+". Configuration information will not be loaded from that file. Remove that file to suppress this message.");
+		}
+	}
+
 }
