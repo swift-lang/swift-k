876bd583ee46e2aef39b0159c2dff8fb2e745ec2
Ben Clifford <benc@hawaga.org.uk>
2008-03-19 06:46:44 +0000

    tests for dryrun and typecheck options
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1741 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/misc/typecheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# run tests with no retries
+# this should expose, for example, the problem i'm seeing with
+# 130-fmri failing once on my mac
+
+export CF=swift.properties.no-retries
+
+cd ../language-behaviour
+
+swift -tc.file ./tc.data -typecheck 061-cattwo.swift
+if [ "$?" -ne "0" ]; then echo "SWIFT RETURN CODE NON-ZERO"; exit 1; fi
+
+
+swift -tc.file ./tc.data -typecheck 130-fmri.swift
+if [ "$?" -ne "0" ]; then echo "SWIFT RETURN CODE NON-ZERO"; exit 1; fi
+
