02504fc2017ee5f0f696f5c2107236fd26f07ce3
Ben Clifford <benc@hawaga.org.uk>
2008-03-26 08:02:35 +0000

    remove never-used path parameter to partialCloseDataset
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1765 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/PartialCloseDataset.java
+++ src/org/griphyn/vdl/karajan/lib/PartialCloseDataset.java
@@ -21,7 +21,7 @@ public class PartialCloseDataset extends VDLFunction {
 	public static final Arg OA_STATEMENTID = new Arg.Optional("closeID", null);
 
 	static {
-		setArguments(PartialCloseDataset.class, new Arg[] { PA_VAR, OA_PATH, OA_STATEMENTID });
+		setArguments(PartialCloseDataset.class, new Arg[] { PA_VAR, OA_STATEMENTID });
 	}
 
 
@@ -31,66 +31,57 @@ public class PartialCloseDataset extends VDLFunction {
 	static Map pendingDatasets = new HashMap();
 
 
-	// TODO path is not used!
 	public Object function(VariableStack stack) throws ExecutionException {
 		boolean hasUnseenToken = false;
-		Path path = parsePath(OA_PATH.getValue(stack), stack);
 		DSHandle var = (DSHandle) PA_VAR.getValue(stack);
 		String statementID = (String) OA_STATEMENTID.getValue(stack);
-		try {
-			if (logger.isInfoEnabled()) {
-				logger.info("Partially closing " + var + " for statement " + statementID);
+		if (logger.isInfoEnabled()) {
+			logger.info("Partially closing " + var + " for statement " + statementID);
+		}
+
+		logger.info("var is "+var);
+		logger.info("var hash is "+var.hashCode());
+
+		if(var.isClosed()) {
+			logger.info("variable already closed - skipping partial close processing");
+			return null;
+		}
+
+		synchronized(pendingDatasets) {
+
+			List c = (List) pendingDatasets.get(var);
+			if(c==null) {
+				c=new ArrayList();
+				pendingDatasets.put(var,c);
 			}
-			var = var.getField(path);
 
-			logger.info("var is "+var);
-			logger.info("var hash is "+var.hashCode());
+			c.add(statementID);
+			logger.info("Adding token "+statementID+" with hash "+statementID.hashCode());
 
-			if(var.isClosed()) {
-				logger.info("variable already closed - skipping partial close processing");
-				return null;
-			}
-
-			synchronized(pendingDatasets) {
-
-				List c = (List) pendingDatasets.get(var);
-				if(c==null) {
-					c=new ArrayList();
-					pendingDatasets.put(var,c);
+			String needToWaitFor = (String) var.getParam("waitfor");
+			logger.info("need to wait for "+needToWaitFor);
+			StringTokenizer stok = new StringTokenizer(needToWaitFor, " ");
+			while(stok.hasMoreTokens()) {
+				String s = stok.nextToken();
+				// do we have this token in our list of already seen
+				// statement IDs?
+				if(! c.contains(s)) {
+					// then we have a required element that we have not
+					// seen yet, so...
+					hasUnseenToken = true;
+					logger.info("Container does not contain token "+s);
+				} else {
+					logger.info("Container does contain token "+s);
 				}
-
-				c.add(statementID);
-				logger.info("Adding token "+statementID+" with hash "+statementID.hashCode());
-
-				String needToWaitFor = (String) var.getParam("waitfor");
-				logger.info("need to wait for "+needToWaitFor);
-				StringTokenizer stok = new StringTokenizer(needToWaitFor, " ");
-				while(stok.hasMoreTokens()) {
-					String s = stok.nextToken();
-					// do we have this token in our list of already seen
-					// statement IDs?
-					if(! c.contains(s)) {
-						// then we have a required element that we have not
-						// seen yet, so...
-						hasUnseenToken = true;
-						logger.info("Container does not contain token "+s);
-					} else {
-
-						logger.info("Container does contain token "+s);
-					}
-				}
-			}
-			logger.info("hasUnseenToken = "+hasUnseenToken);
-			if(!hasUnseenToken) {
-				if(logger.isInfoEnabled()) {
-					logger.info("All partial closes for " + var + " have happened. Closing fully.");
-				}
-				var.closeDeep();
-				pendingDatasets.remove(var);
 			}
 		}
-		catch (InvalidPathException e) {
-			throw new ExecutionException(e);
+		logger.info("hasUnseenToken = "+hasUnseenToken);
+		if(!hasUnseenToken) {
+			if(logger.isInfoEnabled()) {
+				logger.info("All partial closes for " + var + " have happened. Closing fully.");
+			}
+			var.closeDeep();
+			pendingDatasets.remove(var);
 		}
 		return null;
 	}
