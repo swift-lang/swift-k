674ec38590e97582e37781b2a363da9516eb86d0
Mihael Hategan <hategan@mcs.anl.gov>
2008-04-07 08:02:56 +0000

    a security context is not the place to throw such an exception
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@1961 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/provider-webdav/src/org/globus/cog/abstraction/impl/file/webdav/WebDAVSecurityContextImpl.java
+++ cog/modules/provider-webdav/src/org/globus/cog/abstraction/impl/file/webdav/WebDAVSecurityContextImpl.java
@@ -10,7 +10,6 @@ import java.net.PasswordAuthentication;
 import java.util.Hashtable;
 
 import org.apache.log4j.Logger;
-import org.globus.cog.abstraction.impl.common.task.InvalidSecurityContextException;
 import org.globus.cog.abstraction.interfaces.SecurityContext;
 
 public class WebDAVSecurityContextImpl implements SecurityContext {
@@ -41,11 +40,7 @@ public class WebDAVSecurityContextImpl implements SecurityContext {
         }
     }
 
-    public Object getCredentials() throws InvalidSecurityContextException {
-        if (credentials == null) {
-            throw new InvalidSecurityContextException(
-                    "WebDAV provider cannot handle default credentials. Please provide a valid WebDAV credential");
-        }
+    public Object getCredentials() {
         return this.credentials;
     }
 
