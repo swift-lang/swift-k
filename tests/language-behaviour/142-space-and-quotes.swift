3ad3c6da2506dec9f3743c6f38390c08c21d6584
Ben Clifford <benc@hawaga.org.uk>
2008-05-07 18:46:21 +0000

    test with a bunch of quotes that I though I had previously committed
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@1938 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/language-behaviour/142-space-and-quotes.swift
@@ -0,0 +1,24 @@
+
+// this checks how spaces and quotes get passed through to underlying
+// executables
+
+type messagefile;
+
+(messagefile t) p() { 
+    app {
+       touch @filename(t);
+    }
+}
+
+messagefile outfile <"142-space-and-quotes.out">;
+outfile = p();
+
+messagefile outfileB <"142-space-and-quotes. space .out">;
+outfileB = p();
+
+messagefile outfileC <"142-space-and-quotes.2\" space \".out">;
+outfileC = p();
+
+messagefile outfileD <"142-space-and-quotes.3' space '.out">;
+outfileD = p();
+
