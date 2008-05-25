c6fac8c7a4fab8d3123063d613c19c51eb6709e9
Ben Clifford <benc@hawaga.org.uk>
2008-05-25 18:24:31 +0000

    more restart tests
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2005 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/misc/restart5.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+rm -f *.rlog restart-*.out restart5.kml restart5.xml
+rm -rf _concurrent
+
+echo "localhost	helperA	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" > tmp.restartOK.tc.data
+echo "localhost	helperB	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" >> tmp.restartOK.tc.data
+echo "localhost	helperC	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" >> tmp.restartOK.tc.data
+
+swift -tc.file tmp.restartOK.tc.data restart5.swift
+
+PRECHECKEXIT=$?
+
+
+if [ "$PRECHECKEXIT" != 0 ]; then
+  echo Failed - attempt to run workflow without restart configuration failed
+  exit 1
+fi
+
+rm -f *.rlog restart-*.out restart5.kml restart5.xml
+rm -rf _concurrent
+
+echo "localhost	helperA	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" > tmp.restartA.tc.data
+echo "localhost	helperB	$(pwd)/restart-helper-fail	INSTALLED	INTEL32::LINUX	null" >> tmp.restartA.tc.data
+echo "localhost	helperC	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" >> tmp.restartA.tc.data
+
+swift -tc.file tmp.restartA.tc.data restart5.swift
+
+FIRSTEXIT=$?
+
+# this invocation should fail, with restart-1.out in existence but
+# not the others
+
+if [ "$FIRSTEXIT" == 0 ]; then
+  echo Failed - workflow was indicated as successfully completed the first time round.
+  exit 1
+fi
+
+# now make A fail - we should have run it already, and so we want to make
+# sure it does not run again; and make B succeed this time round.
+
+echo "localhost	helperA	$(pwd)/restart-helper-fail	INSTALLED	INTEL32::LINUX	null" > tmp.restartB.tc.data
+echo "localhost	helperB	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" >> tmp.restartB.tc.data
+echo "localhost	helperC	$(pwd)/restart-helper-success	INSTALLED	INTEL32::LINUX	null" >> tmp.restartB.tc.data
+
+# there should be only a single rlog here, because we deleted them all
+# at the start of this script.
+swift -resume *.rlog -tc.file tmp.restartB.tc.data restart5.swift
+
+SECONDEXIT=$?
+
+if [ "$SECONDEXIT" != "0" ]; then
+  echo Failed - second round failed
+  exit 4
+fi
+
+echo success
+exit 0
+
---
+++ tests/misc/restart5.swift
@@ -0,0 +1,43 @@
+type file;                                                                      
+                                                                                
+(file t) a(file i) {                                                            
+    app {                                                                       
+        helperA @filename(t);                                                   
+    }                                                                           
+}                                                                               
+                                                                                
+(file t) b(file i) {                                                            
+    app {                                                                       
+        helperB @filename(t);                                                   
+    }                                                                           
+}                                                                               
+                                                                                
+(file t) c(file i) {                                                            
+    app {                                                                       
+        helperC @filename(t);                                                   
+    }          
+}                                                                               
+                                                                                
+(file r) q(file i, int n) {                                                     
+file t;                                                                         
+switch(n) {                                                                     
+case 1: t=a(i); r=c(t);                                                         
+case 2: t=b(i); r=c(t);                                                         
+case 3: t=c(i); r=c(t);                                                         
+}                                                                               
+}                                                                               
+                                                                                
+file J <"restart.in">;                                                          
+                                                                                
+file X[];                                                                       
+                                                                                
+file Y[];   
+                                                                             
+foreach i in [1:3] {                                                            
+  X[i] = q(J,i);                                                                
+}                                                                               
+                                                                                
+foreach x,j in X {                                                              
+Y[j] = q(x,j);                                                                  
+}                                                                               
+                                                                                
