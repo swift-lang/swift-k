swift-lang/swift-k

commit a44971620a7a019b46e32c89aad52dcea43528e0

hopefully better behaviour in the presence of bad sites
git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2058 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHost.java
@@ -11,6 +11,7 @@ package org.globus.cog.karajan.scheduler;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import org.apache.log4j.Logger;
 
 import org.globus.cog.karajan.util.BoundContact;
 
@@ -18,12 +19,15 @@ public class WeightedHost implements Comparable {
 
 	static final int MINWEIGHT = -10;
 
+	private static final Logger logger = Logger.getLogger(WeightedHost.class);
+
 	private BoundContact host;
 	private Double score;
 	private double tscore;
 	private int load;
 	private double delayedDelta;
 	private float jobThrottle;
+	private long lastUsed;
 
 	public WeightedHost(BoundContact contact, float jobThrottle) {
 		this(contact, 0.0, jobThrottle);
@@ -63,7 +67,8 @@ public class WeightedHost implements Comparable {
 	}
 
 	public final double getTScore() {
-		return tscore;
+		if(tscore >= 1) return tscore;
+		if(isOverloaded()) return 0; else return 1;
 	}
 
 	public final BoundContact getHost() {
@@ -130,7 +135,25 @@ public class WeightedHost implements Comparable {
 	}
 
 	public boolean isOverloaded() {
-		return !(load < maxLoad());
+		double ml = maxLoad();
+		if(tscore >= 1) {
+			// the site is mostly good. permit 1 or more jobs
+			// always.
+			logger.info("In load mode. score = "+score+" tscore = "+tscore+", maxload="+ml);
+			return !(load <= ml);
+		} else {
+			// the site is mostly bad. allow either 1 or 0 jobs
+			// based on time.
+			long now = System.currentTimeMillis();
+			long delay = now - lastUsed;
+			long permittedDelay = (long)(Math.exp(-(score.doubleValue()))*100.0);
+			boolean overloaded=(delay<permittedDelay);
+			// tscore of -1 will give delay of around 
+			// 200ms, and will double every time tscore goes
+			// down by one (which is once per failed job? roughly?)
+			logger.info("In delay mode. score = "+score+" tscore = "+tscore+", maxload="+ml+" delay since last used="+delay+"ms"+" permitted delay="+permittedDelay+"ms overloaded="+overloaded);
+			return overloaded;
+		}
 	}
 
 	public float getJobThrottle() {
@@ -138,6 +161,10 @@ public class WeightedHost implements Comparable {
 	}
 
 	public double maxLoad() {
-		return jobThrottle * tscore + 2;
+		return jobThrottle * tscore + 1;
+	}
+
+	public void notifyUsed() {
+		lastUsed = System.currentTimeMillis();
 	}
 }

modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHostScoreScheduler.java
@@ -223,6 +223,7 @@ public class WeightedHostScoreScheduler extends LateBindingScheduler {
 
 		sorted.changeLoad(selected, 1);
 		selected.setDelayedDelta(successFactor);
+		selected.notifyUsed();
 		return selected.getHost();
 	}
 

commit 1137821479d2a360df50ece1ad10335574a698ed

overload-chk2 patch from Mihael; fixes a problem introduced in r2058
git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2065 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHost.java
@@ -11,8 +11,9 @@ package org.globus.cog.karajan.scheduler;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import org.apache.log4j.Logger;
+import java.util.Timer;
 
+import org.apache.log4j.Logger;
 import org.globus.cog.karajan.util.BoundContact;
 
 public class WeightedHost implements Comparable {
@@ -70,7 +71,7 @@ public class WeightedHost implements Comparable {
 	public final double getTScore() {
 		if (tscore >= 1)
 			return tscore;
-		if (isOverloaded())
+		if (isOverloaded() != 0)
 			return 0;
 		else
 			return 1;
@@ -116,7 +117,7 @@ public class WeightedHost implements Comparable {
 
 	public String toString() {
 		return host.toString() + ":" + D4.format(score) + "(" + D4.format(tscore) + "):" + load
-				+ "/" + (int) (maxLoad()) + (isOverloaded() ? " overloaded" : "");
+				+ "/" + (int) (maxLoad()) + " overload: " + isOverloaded();
 	}
 
 	public int compareTo(Object o) {
@@ -139,7 +140,7 @@ public class WeightedHost implements Comparable {
 		this.delayedDelta = delayedDelta;
 	}
 
-	public boolean isOverloaded() {
+	public int isOverloaded() {
 		double ml = maxLoad();
 		if (tscore >= 1) {
 			// the site is mostly good. permit 1 or more jobs
@@ -148,7 +149,7 @@ public class WeightedHost implements Comparable {
 				logger.debug("In load mode. score = " + score + " tscore = " + tscore + ", maxload="
 						+ ml);
 			}
-			return !(load <= ml);
+			return load <= ml ? 0 : 1;
 		}
 		else {
 			// the site is mostly bad. allow either 1 or 0 jobs
@@ -165,7 +166,7 @@ public class WeightedHost implements Comparable {
 						+ ", maxload=" + ml + " delay since last used=" + delay + "ms"
 						+ " permitted delay=" + permittedDelay + "ms overloaded=" + overloaded);
 			}
-			return overloaded;
+			return (int) (delay - permittedDelay);
 		}
 	}
 

modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHostScoreScheduler.java
@@ -274,7 +274,7 @@ public class WeightedHostScoreScheduler extends LateBindingScheduler {
 			Iterator i = s.iterator();
 			while (i.hasNext()) {
 				WeightedHost wh = (WeightedHost) i.next();
-				if (!wh.isOverloaded()) {
+				if (wh.isOverloaded() == 0) {
 					ns.add(wh);
 				}
 			}
@@ -284,7 +284,7 @@ public class WeightedHostScoreScheduler extends LateBindingScheduler {
 			Iterator i = s.iterator();
 			while (i.hasNext()) {
 				WeightedHost wh = (WeightedHost) i.next();
-				if (wh.isOverloaded()) {
+				if (wh.isOverloaded() != 0) {
 					i.remove();
 				}
 			}

modules/karajan/src/org/globus/cog/karajan/scheduler/WeightedHostSet.java
@@ -12,6 +12,8 @@ package org.globus.cog.karajan.scheduler;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.TreeSet;
 
 import org.globus.cog.karajan.util.BoundContact;
@@ -21,7 +23,9 @@ public class WeightedHostSet {
 	private Map weightedHosts;
 	private double sum;
 	private double scoreHighCap;
-	private int overloadedCount;
+	private volatile int overloadedCount;
+
+	private static final Timer timer = new Timer();
 
 	public WeightedHostSet(double scoreHighCap) {
 		init();
@@ -38,32 +42,52 @@ public class WeightedHostSet {
 		scores.add(wh);
 		weightedHosts.put(wh.getHost(), wh);
 		sum += wh.getTScore();
-		overloadedCount += wh.isOverloaded() ? 1 : 0;
+		overloadedCount += checkOverloaded(wh, 1);
 	}
 
 	public void changeScore(WeightedHost wh, double newScore) {
 		scores.remove(wh);
 		sum -= wh.getTScore();
-		overloadedCount -= wh.isOverloaded() ? 1 : 0;
+		overloadedCount += checkOverloaded(wh, -1);
 		wh.setScore(newScore);
 		weightedHosts.put(wh.getHost(), wh);
 		scores.add(wh);
 		sum += wh.getTScore();
-		overloadedCount += wh.isOverloaded() ? 1 : 0;
+		overloadedCount += checkOverloaded(wh, 1);
 	}
 
 	public void changeLoad(WeightedHost wh, int dl) {
-		overloadedCount -= wh.isOverloaded() ? 1 : 0;
+		overloadedCount -= checkOverloaded(wh, -1);
 		wh.changeLoad(dl);
-		overloadedCount += wh.isOverloaded() ? 1 : 0;
+		overloadedCount += checkOverloaded(wh, 1);
 	}
 
 	public double remove(WeightedHost wh) {
 		scores.remove(wh);
 		weightedHosts.remove(wh.getHost());
 		sum -= wh.getScore();
-		overloadedCount -= wh.isOverloaded() ? 1 : 0;
+		overloadedCount += checkOverloaded(wh, -1);
 		return wh.getScore();
+	}
+
+	private int checkOverloaded(WeightedHost wh, final int dir) {
+		int v = wh.isOverloaded();
+		if (v >= 0) {
+		    if (dir > 0) {
+		    	return v;
+		    }
+		    else {
+		        return -v;
+		    }
+		}
+		else {
+			timer.schedule(new TimerTask() {
+				public void run() {
+				    overloadedCount -= dir;
+				}
+			}, -v);
+			return dir;
+		}
 	}
 
 	public WeightedHost findHost(BoundContact bc) {