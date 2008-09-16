c26dad7f875efc4f13cb7537344a76a27f603284
Mihael Hategan <hategan@mcs.anl.gov>
2008-09-16 17:40:55 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2162 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/local/LocalRequestManager.java
+++ modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/local/LocalRequestManager.java
@@ -11,13 +11,17 @@ package org.globus.cog.abstraction.coaster.service.local;
 
 import org.globus.cog.karajan.workflow.service.AbstractRequestManager;
 import org.globus.cog.karajan.workflow.service.handlers.ChannelConfigurationHandler;
+import org.globus.cog.karajan.workflow.service.handlers.HeartBeatHandler;
 
-public class LocalRequestManager extends AbstractRequestManager {   
+public class LocalRequestManager extends AbstractRequestManager {
+    public static final LocalRequestManager INSTANCE = new LocalRequestManager();
+    
     public LocalRequestManager() {
         addHandler(VersionHandler.NAME, VersionHandler.class);
         addHandler(RegistrationHandler.NAME, RegistrationHandler.class);
         addHandler(UnregisterHandler.NAME, UnregisterHandler.class);
         addHandler("CHANNELCONFIG", ChannelConfigurationHandler.class);
         addHandler(JobStatusHandler.NAME, JobStatusHandler.class);
+        addHandler(HeartBeatHandler.NAME, HeartBeatHandler.class);
     }
 }
