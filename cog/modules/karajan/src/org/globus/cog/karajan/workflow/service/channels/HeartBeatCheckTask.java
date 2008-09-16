c47b402f760c6729fd44964886914b5a2f231ed0
Mihael Hategan <hategan@mcs.anl.gov>
2008-09-16 17:48:48 +0000

    added heartbeat command/handler
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@2167 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/channels/HeartBeatCheckTask.java
@@ -0,0 +1,41 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Sep 9, 2008
+ */
+package org.globus.cog.karajan.workflow.service.channels;
+
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+
+public class HeartBeatCheckTask extends TimerTask {
+	public static final Logger logger = Logger.getLogger(HeartBeatCheckTask.class);
+
+	private KarajanChannel channel;
+	private int multiplier, interval;
+
+	public HeartBeatCheckTask(KarajanChannel channel, int interval, int multiplier) {
+		this.channel = channel;
+		this.multiplier = multiplier;
+		this.interval = interval;
+		channel.getChannelContext().setLastHeartBeat(System.currentTimeMillis());
+	}
+
+	public void run() {
+		if (channel.isOffline()) {
+			this.cancel();
+		}
+		else if (channel.isStarted()) {
+			if (channel.getChannelContext().getLastHeartBeat() - interval * multiplier > System.currentTimeMillis()) {
+				logger.warn("Channel (" + channel + ") has not received any heartbeat in "
+						+ multiplier + " intervals. Shutting it down.");
+				channel.shutdown();
+			}
+		}
+	}
+}
