56aee5d08a2a90dbc20ea37881b9ebcd7df3421f
Ben Clifford <benc@hawaga.org.uk>
2008-09-22 23:11:16 +0000

    better sites file for replication test
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2243 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/misc/wonky-replication.sh
+++ tests/misc/wonky-replication.sh
@@ -16,11 +16,17 @@ cat tc.data ../sites/tc.data > tmp.tc.data.sites
 cat tmp.tc.data.wonky-twosite | sed 's/localhost/wonkyA/' >> tmp.tc.data.sites
 cat tmp.tc.data.wonky-twosite | sed 's/localhost/wonkyB/' >> tmp.tc.data.sites
 
-cat $(dirname $(which swift))/../etc/swift.properties | grep --invert-match -E '^replication.enabled=' > $CF
+cat $(dirname $(which swift))/../etc/swift.properties | \
+ grep --invert-match -E '^replication.enabled=' | \
+ grep --invert-match -E '^tc.file=' | \
+ grep --invert-match -E '^sites.file=' \
+ > $CF
 echo replication.enabled=true >> $CF
+echo tc.file=tmp.tc.data.sites >> $CF
+echo sites.file=../sites/wonky/slow-queue-fast-queue.xml >> $CF
 
-export SWIFT_TEST_PARAMS="-config $CF -tc.file tmp.tc.data.sites -sites.file ../sites/wonky/slow-queue-fast-queue.xml -debug" 
+export SWIFT_TEST_PARAMS="-config $CF" 
 
-./run 066-many.swift
+./run 0651-several-delay.swift 066-many.swift
 
 
