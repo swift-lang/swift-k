a80fa4b7813197af35f1823348641c3e2238bfce
Mihael Hategan <hategan@mcs.anl.gov>
2008-09-30 16:54:56 +0000

    added relevant things to request manager
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2206 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/CoasterRequestManager.java
+++ modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/CoasterRequestManager.java
@@ -10,6 +10,26 @@
 package org.globus.cog.abstraction.coaster.service;
 
 import org.globus.cog.abstraction.impl.execution.coaster.SubmitJobCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.ChmodCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.ExistsCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.FileInfoCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.GetFileCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.IsDirectoryCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.ListCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.MkdirCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.PutFileCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.RenameCommand;
+import org.globus.cog.abstraction.impl.file.coaster.commands.RmdirCommand;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.ChmodHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.ExistsHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.FileInfoHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.GetFileHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.IsDirectoryHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.ListHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.MkdirHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.PutFileHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.RenameHandler;
+import org.globus.cog.abstraction.impl.file.coaster.handlers.RmdirHandler;
 import org.globus.cog.karajan.workflow.service.AbstractRequestManager;
 import org.globus.cog.karajan.workflow.service.handlers.ChannelConfigurationHandler;
 import org.globus.cog.karajan.workflow.service.handlers.ShutdownHandler;
@@ -22,5 +42,16 @@ public class CoasterRequestManager extends AbstractRequestManager {
         addHandler("SHUTDOWN", ShutdownHandler.class);
         addHandler(SubmitJobCommand.NAME, SubmitJobHandler.class);
         addHandler(ServiceShutdownHandler.NAME, ServiceShutdownHandler.class);
+        
+        addHandler(ExistsCommand.NAME, ExistsHandler.class);
+        addHandler(MkdirCommand.NAME, MkdirHandler.class);
+        addHandler(ListCommand.NAME, ListHandler.class);
+        addHandler(RmdirCommand.NAME, RmdirHandler.class);
+        addHandler(FileInfoCommand.NAME, FileInfoHandler.class);
+        addHandler(GetFileCommand.NAME, GetFileHandler.class);
+        addHandler(PutFileCommand.NAME, PutFileHandler.class);
+        addHandler(ChmodCommand.NAME, ChmodHandler.class);
+        addHandler(RenameCommand.NAME, RenameHandler.class);
+        addHandler(IsDirectoryCommand.NAME, IsDirectoryHandler.class);
     }
 }
