22c6b15cda0f239a4f66746ebc2d53a502130338
Mihael Hategan <hategan@mcs.anl.gov>
2008-10-02 18:42:31 +0000

    updated handlers
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2213 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/provider-coaster/src/org/globus/cog/abstraction/impl/file/coaster/handlers/ListHandler.java
+++ modules/provider-coaster/src/org/globus/cog/abstraction/impl/file/coaster/handlers/ListHandler.java
@@ -10,27 +10,47 @@
 package org.globus.cog.abstraction.impl.file.coaster.handlers;
 
 import java.io.File;
+import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.globus.cog.abstraction.interfaces.GridFile;
 import org.globus.cog.karajan.workflow.service.ProtocolException;
 
 public class ListHandler extends CoasterFileRequestHandler {
+    public static final Logger logger = Logger.getLogger(ListHandler.class);
 
     public void requestComplete() throws ProtocolException {
         File f = normalize(getInDataAsString(0));
-
-        File[] l = f.listFiles();
-        for (int i = 0; i < l.length; i++) {
-            f = l[i];
-            addOutData(f.getAbsolutePath());
-            addOutData(f.isDirectory() ? GridFile.DIRECTORY : GridFile.FILE);
-            addOutData("");
-            addOutData(f.length());
-            addOutData(0);
-            addOutData(0);
-            addOutData(0);
+        if (logger.isInfoEnabled()) {
+            logger.info("Listing files in " + f.getAbsolutePath());
         }
-        sendReply();
+        File[] l = f.listFiles();
+        if (l == null) {
+            if (logger.isInfoEnabled()) {
+                logger.info("Invalid directory: " + f.getAbsolutePath());
+            }
+            sendError("No such directory: " + f.getAbsolutePath());
+        }
+        else {
+            if (logger.isInfoEnabled()) {
+                logger.info("Listing " + l.length + " files");
+            }
+            for (int i = 0; i < l.length; i++) {
+                f = l[i];
+                addOutData(f.getAbsolutePath());
+                addOutData(new Date().toString());
+                addOutData(f.isDirectory() ? GridFile.DIRECTORY
+                        : GridFile.FILE);
+                addOutData(f.length());
+                addOutData(0);
+                addOutData(0);
+                addOutData(0);
+            }
+            if (logger.isInfoEnabled()) {
+                logger.info("Sending reply");
+            }
+            sendReply();
+        }
     }
 
 }
