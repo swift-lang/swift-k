swift-lang/swift-k

commit b5da95bf98a65319a3f36171b93dbfb621d38e1e

If supplied executable is not an absolute path, look up the path using "which"
git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2226 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

libexec/wrapper.sh
@@ -163,6 +163,10 @@ if [ "$PATHPREFIX" != "" ]; then
 export PATH=$PATHPREFIX:$PATH
 fi
 
+if [ "X${EXEC:1:1}" != "X/" ] ; then
+export EXEC=$(which $EXEC)
+fi
+
 log "DIR=$DIR"
 log "EXEC=$EXEC"
 log "STDIN=$STDIN"

commit 6ab767210055ec807ec23d900aa92c22b92ad793

off-by-one error, introduced in r2226. this was breaking things in the case of tguc + coasters + gram4 (but not gram2).
git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2286 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

libexec/wrapper.sh
@@ -163,7 +163,7 @@ if [ "$PATHPREFIX" != "" ]; then
 export PATH=$PATHPREFIX:$PATH
 fi
 
-if [ "X${EXEC:1:1}" != "X/" ] ; then
+if [ "X${EXEC:0:1}" != "X/" ] ; then
 export EXEC=$(which $EXEC)
 fi
 