3bd9b30c1b7ccaa70bcb6e183a9b56744dc445dd
Ben Clifford <benc@hawaga.org.uk>
2009-02-06 22:22:00 +0000

    r2498 forgot to add this file
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2506 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ src/org/griphyn/vdl/karajan/lib/ExpandArguments.java
@@ -0,0 +1,72 @@
+package org.griphyn.vdl.karajan.lib;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.Set;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.globus.cog.karajan.arguments.Arg;
+import org.globus.cog.karajan.stack.VariableStack;
+import org.globus.cog.karajan.workflow.ExecutionException;
+import org.globus.cog.karajan.workflow.futures.FutureNotYetAvailable;
+
+import org.griphyn.vdl.mapping.ArrayDataNode;
+import org.griphyn.vdl.mapping.DSHandle;
+import org.griphyn.vdl.mapping.HandleOpenException;
+import org.griphyn.vdl.mapping.InvalidPathException;
+import org.griphyn.vdl.mapping.Path;
+
+public class ExpandArguments extends VDLFunction {
+	public static final Logger logger = Logger.getLogger(ExpandArguments.class);
+
+	static {
+		setArguments(ExpandArguments.class, new Arg[] { Arg.VARGS });
+	}
+
+	public Object function(VariableStack stack) throws ExecutionException {
+		ArrayList l = new ArrayList();
+		Object[] items = Arg.VARGS.asArray(stack);
+		for (int i = 0; i < items.length; i++) {
+			Object item = items[i];
+			if(!(item instanceof DSHandle)) {
+				throw new RuntimeException("Cannot handle argument implemented by "+item.getClass());
+			}
+
+			if(item instanceof ArrayDataNode) {
+				ArrayDataNode array = (ArrayDataNode) item;
+				Map m=array.getArrayValue();
+				Set keySet = m.keySet();
+				TreeSet sortedKeySet = new TreeSet(new StringsAsIntegersComparator());
+				sortedKeySet.addAll(keySet);
+				Iterator it = sortedKeySet.iterator();
+				while(it.hasNext()) {
+					Object key = it.next();
+					l.add(m.get(key));
+				}
+			} else {
+                       		l.add(item);
+			}
+			// TODO this does not correctly handle structs or
+			// externals - at the moment, probably neither of
+			// those should be usable as a string. It also
+			// does not handle nested arrays. However, none of
+			// those should get here in normal operation due
+			// to static type-checking
+                }
+		return l;
+	}
+
+	class StringsAsIntegersComparator implements Comparator {
+		public int compare(Object l, Object r) {
+			Integer lnum = new Integer((String)l);
+			Integer rnum = new Integer((String)r);
+			return lnum.compareTo(rnum);
+		}
+	}
+
+}
+
