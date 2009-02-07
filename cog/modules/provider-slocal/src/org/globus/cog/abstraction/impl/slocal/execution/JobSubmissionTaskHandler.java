1768eeceb7b98db0b86c203006e4598920ad98b3
Mihael Hategan <hategan@mcs.anl.gov>
2009-02-07 02:41:59 +0000

    cancel message; cleanup; updated version
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@2287 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/provider-slocal/src/org/globus/cog/abstraction/impl/slocal/execution/JobSubmissionTaskHandler.java
+++ cog/modules/provider-slocal/src/org/globus/cog/abstraction/impl/slocal/execution/JobSubmissionTaskHandler.java
@@ -7,6 +7,7 @@
 package org.globus.cog.abstraction.impl.slocal.execution;
 
 import org.apache.log4j.Logger;
+import org.globus.cog.abstraction.impl.common.AbstractDelegatedTaskHandler;
 import org.globus.cog.abstraction.impl.common.StatusImpl;
 import org.globus.cog.abstraction.impl.common.task.IllegalSpecException;
 import org.globus.cog.abstraction.impl.common.task.InvalidSecurityContextException;
@@ -14,7 +15,6 @@ import org.globus.cog.abstraction.impl.common.task.InvalidServiceContactExceptio
 import org.globus.cog.abstraction.impl.common.task.TaskSubmissionException;
 import org.globus.cog.abstraction.impl.slocal.Executor;
 import org.globus.cog.abstraction.impl.slocal.ProcessListener;
-import org.globus.cog.abstraction.interfaces.DelegatedTaskHandler;
 import org.globus.cog.abstraction.interfaces.JobSpecification;
 import org.globus.cog.abstraction.interfaces.Status;
 import org.globus.cog.abstraction.interfaces.Task;
@@ -23,62 +23,49 @@ import org.globus.cog.abstraction.interfaces.Task;
  * @author Kaizar Amin (amin@mcs.anl.gov)
  * 
  */
-public class JobSubmissionTaskHandler implements DelegatedTaskHandler, ProcessListener {
+public class JobSubmissionTaskHandler extends AbstractDelegatedTaskHandler implements ProcessListener {
 
 	private static Logger logger = Logger.getLogger(JobSubmissionTaskHandler.class);
 
-	private Task task = null;
 	private Thread thread = null;
 
 	public void submit(Task task) throws IllegalSpecException, InvalidSecurityContextException,
 			InvalidServiceContactException, TaskSubmissionException {
-		if (this.task != null) {
-			throw new TaskSubmissionException(
-					"JobSubmissionTaskHandler cannot handle two active jobs simultaneously");
+		checkAndSetTask(task);
+		JobSpecification spec;
+		try {
+			spec = (JobSpecification) task.getSpecification();
 		}
-		else {
-			this.task = task;
-			JobSpecification spec;
-			try {
-				spec = (JobSpecification) this.task.getSpecification();
-			}
-			catch (Exception e) {
-				throw new IllegalSpecException("Exception while retreiving Job Specification", e);
-			}
+		catch (Exception e) {
+			throw new IllegalSpecException("Exception while retreiving Job Specification", e);
+		}
 
-			if (task.getAllServices() == null || task.getAllServices().size() == 0
-					|| task.getService(0) == null) {
-				throw new InvalidSecurityContextException("No service specified");
-			}
-			if (task.getService(0).getSecurityContext() == null
-					|| task.getService(0).getSecurityContext().getCredentials() == null) {
-				throw new InvalidSecurityContextException("No credentials supplied");
-			}
+		if (task.getAllServices() == null || task.getAllServices().size() == 0
+				|| task.getService(0) == null) {
+			throw new InvalidSecurityContextException("No service specified");
+		}
+		if (task.getService(0).getSecurityContext() == null
+				|| task.getService(0).getSecurityContext().getCredentials() == null) {
+			throw new InvalidSecurityContextException("No credentials supplied");
+		}
 
-			try {
-				new Executor(task, task.getService(0).getSecurityContext().getCredentials(), this).start();
-				// check if the task has not been canceled after it was
-				// submitted for execution
-				if (this.task.getStatus().getStatusCode() == Status.UNSUBMITTED) {
-					this.task.setStatus(Status.SUBMITTED);
-					if (spec.isBatchJob()) {
-						this.task.setStatus(Status.COMPLETED);
-					}
+		try {
+			new Executor(task, task.getService(0).getSecurityContext().getCredentials(), this).start();
+			// check if the task has not been canceled after it was
+			// submitted for execution
+			if (task.getStatus().getStatusCode() == Status.UNSUBMITTED) {
+				task.setStatus(Status.SUBMITTED);
+				if (spec.isBatchJob()) {
+					task.setStatus(Status.COMPLETED);
 				}
 			}
-			catch (Exception e) {
-				Status newStatus = new StatusImpl();
-				Status oldStatus = this.task.getStatus();
-				newStatus.setPrevStatusCode(oldStatus.getStatusCode());
-				newStatus.setStatusCode(Status.FAILED);
-				newStatus.setException(e);
-				this.task.setStatus(newStatus);
-				if (e.getMessage() != null) {
-					throw new TaskSubmissionException("Cannot submit job: " + e.getMessage(), e);
-				}
-				else {
-					throw new TaskSubmissionException("Cannot submit job", e);
-				}
+		}
+		catch (Exception e) {
+			if (e.getMessage() != null) {
+				throw new TaskSubmissionException("Cannot submit job: " + e.getMessage(), e);
+			}
+			else {
+				throw new TaskSubmissionException("Cannot submit job", e);
 			}
 		}
 	}
@@ -89,28 +76,22 @@ public class JobSubmissionTaskHandler implements DelegatedTaskHandler, ProcessLi
 	public void resume() throws InvalidSecurityContextException, TaskSubmissionException {
 	}
 
-	public void cancel() throws InvalidSecurityContextException, TaskSubmissionException {
-		this.task.setStatus(Status.CANCELED);
+	public void cancel(String message) throws InvalidSecurityContextException, TaskSubmissionException {
+		getTask().setStatus(new StatusImpl(Status.CANCELED, message, null));
 	}
 
 	public void processCompleted(int exitCode) {
-		if (task.getStatus().getStatusCode() != Status.FAILED) {
+		if (getTask().getStatus().getStatusCode() != Status.FAILED) {
 			if (exitCode == 0) {
-				task.setStatus(Status.COMPLETED);
+				getTask().setStatus(Status.COMPLETED);
 			}
 			else {
-				Status s = new StatusImpl();
-				s.setMessage("Process failed with exit code " + exitCode);
-				s.setStatusCode(Status.FAILED);
-				task.setStatus(s);
+			    failTask("Process failed with exit code " + exitCode, null);
 			}
 		}
 	}
 
 	public void processFailed(String message) {
-		Status s = new StatusImpl();
-		s.setMessage(message);
-		s.setStatusCode(Status.FAILED);
-		task.setStatus(s);
+	    failTask(message, null);
 	}
 }
