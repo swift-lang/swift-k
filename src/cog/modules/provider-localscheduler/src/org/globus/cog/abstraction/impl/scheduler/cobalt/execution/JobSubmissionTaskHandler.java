04b842ce5e2cdee95ff08f34ca3e24d8eb2cc34e
Mihael Hategan <hategan@mcs.anl.gov>
2009-02-07 02:24:58 +0000

    cancel message; cleanup; updated version
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2285 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-localscheduler/src/org/globus/cog/abstraction/impl/scheduler/cobalt/execution/JobSubmissionTaskHandler.java
+++ src/cog/modules/provider-localscheduler/src/org/globus/cog/abstraction/impl/scheduler/cobalt/execution/JobSubmissionTaskHandler.java
@@ -7,6 +7,7 @@
 package org.globus.cog.abstraction.impl.scheduler.cobalt.execution;
 
 import org.apache.log4j.Logger;
+import org.globus.cog.abstraction.impl.common.AbstractDelegatedTaskHandler;
 import org.globus.cog.abstraction.impl.common.StatusImpl;
 import org.globus.cog.abstraction.impl.common.execution.JobException;
 import org.globus.cog.abstraction.impl.common.task.IllegalSpecException;
@@ -16,68 +17,61 @@ import org.globus.cog.abstraction.impl.common.task.TaskSubmissionException;
 import org.globus.cog.abstraction.impl.scheduler.cobalt.CobaltExecutor;
 import org.globus.cog.abstraction.impl.scheduler.common.Job;
 import org.globus.cog.abstraction.impl.scheduler.common.ProcessListener;
-import org.globus.cog.abstraction.interfaces.DelegatedTaskHandler;
 import org.globus.cog.abstraction.interfaces.FileLocation;
 import org.globus.cog.abstraction.interfaces.JobSpecification;
 import org.globus.cog.abstraction.interfaces.Status;
 import org.globus.cog.abstraction.interfaces.Task;
 
-public class JobSubmissionTaskHandler implements DelegatedTaskHandler,
+public class JobSubmissionTaskHandler extends AbstractDelegatedTaskHandler implements 
         ProcessListener {
 
     private static Logger logger = Logger
             .getLogger(JobSubmissionTaskHandler.class);
 
-    private Task task;
     private JobSpecification spec;
     private Thread thread;
 
     public void submit(Task task) throws IllegalSpecException,
             InvalidSecurityContextException, InvalidServiceContactException,
             TaskSubmissionException {
-        if (this.task != null) {
-            throw new TaskSubmissionException(
-                    "JobSubmissionTaskHandler cannot handle two active jobs simultaneously");
+        checkAndSetTask(task);
+        
+        task.setStatus(Status.SUBMITTING);
+        try {
+            spec = (JobSpecification) task.getSpecification();
         }
-        else {
-            this.task = task;
-            task.setStatus(Status.SUBMITTING);
-            try {
-                spec = (JobSpecification) this.task.getSpecification();
-            }
-            catch (Exception e) {
-                throw new IllegalSpecException(
-                        "Exception while retreiving Job Specification", e);
-            }
+        catch (Exception e) {
+            throw new IllegalSpecException(
+                    "Exception while retreiving Job Specification", e);
+        }
 
-            if (task.getAllServices() == null
-                    || task.getAllServices().size() == 0
-                    || task.getService(0) == null) {
-                throw new InvalidSecurityContextException(
-                        "No service specified");
-            }
+        if (task.getAllServices() == null
+                || task.getAllServices().size() == 0
+                || task.getService(0) == null) {
+            throw new InvalidSecurityContextException(
+                    "No service specified");
+        }
 
-            try {
-                synchronized(this) {                    
-                    // check if the task has not been canceled after it was
-                    // submitted for execution
-                    if (this.task.getStatus().getStatusCode() != Status.CANCELED) {
-                        new CobaltExecutor(task, this).start();
-                        this.task.setStatus(Status.SUBMITTED);
-                        if (spec.isBatchJob()) {
-                            this.task.setStatus(Status.COMPLETED);
-                        }
+        try {
+            synchronized(this) {                    
+                // check if the task has not been canceled after it was
+                // submitted for execution
+                if (task.getStatus().getStatusCode() != Status.CANCELED) {
+                    new CobaltExecutor(task, this).start();
+                    task.setStatus(Status.SUBMITTED);
+                    if (spec.isBatchJob()) {
+                        task.setStatus(Status.COMPLETED);
                     }
                 }
             }
-            catch (Exception e) {
-                if (e.getMessage() != null) {
-                    throw new TaskSubmissionException("Cannot submit job: "
-                            + e.getMessage(), e);
-                }
-                else {
-                    throw new TaskSubmissionException("Cannot submit job", e);
-                }
+        }
+        catch (Exception e) {
+            if (e.getMessage() != null) {
+                throw new TaskSubmissionException("Cannot submit job: "
+                        + e.getMessage(), e);
+            }
+            else {
+                throw new TaskSubmissionException("Cannot submit job", e);
             }
         }
     }
@@ -90,56 +84,46 @@ public class JobSubmissionTaskHandler implements DelegatedTaskHandler,
             TaskSubmissionException {
     }
 
-    public synchronized void cancel() throws InvalidSecurityContextException,
+    public synchronized void cancel(String message) throws InvalidSecurityContextException,
             TaskSubmissionException {
         //TODO qdel?
-        this.task.setStatus(Status.CANCELED);
+        getTask().setStatus(new StatusImpl(Status.CANCELED, message, null));
     }
 
     public void processCompleted(int exitCode) {
-        if (task.getStatus().getStatusCode() != Status.FAILED) {
+        if (getTask().getStatus().getStatusCode() != Status.FAILED) {
             if (exitCode == 0) {
-                task.setStatus(Status.COMPLETED);
+                getTask().setStatus(Status.COMPLETED);
             }
             else {
-                Status s = new StatusImpl();
-                s.setException(new JobException(exitCode));
-                s.setStatusCode(Status.FAILED);
-                task.setStatus(s);
+                failTask(null, new JobException(exitCode));
             }
         }
     }
 
     public void processFailed(String message) {
-        Status s = new StatusImpl();
-        s.setMessage(message);
-        s.setStatusCode(Status.FAILED);
-        task.setStatus(s);
+        failTask(message, null);
     }
 
     public void processFailed(Exception e) {
-        Status s = new StatusImpl();
-        s.setMessage(e.getMessage());
-        s.setException(e);
-        s.setStatusCode(Status.FAILED);
-        task.setStatus(s);
+        failTask(null, e);
     }
 
     public void statusChanged(int status) {
         if (status == Job.STATE_RUNNING) {
-            task.setStatus(Status.ACTIVE);
+            getTask().setStatus(Status.ACTIVE);
         }
     }
 
     public void stderrUpdated(String stderr) {
         if (FileLocation.MEMORY.overlaps(spec.getStdErrorLocation())) {
-            task.setStdError(stderr);
+            getTask().setStdError(stderr);
         }
     }
 
     public void stdoutUpdated(String stdout) {
         if (FileLocation.MEMORY.overlaps(spec.getStdOutputLocation())) {
-            task.setStdOutput(stdout);
+            getTask().setStdOutput(stdout);
         }
     }
 }
