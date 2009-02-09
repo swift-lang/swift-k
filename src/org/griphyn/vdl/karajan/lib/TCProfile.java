3218dcdf9a5c717435c5eae702a18597d1997a56
Ben Clifford <benc@hawaga.org.uk>
2009-02-09 09:05:52 +0000

    revert r2509: it breaks gram2 job submission, and appears unnecessary for core swift.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2515 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/TCProfile.java
+++ src/org/griphyn/vdl/karajan/lib/TCProfile.java
@@ -67,7 +67,6 @@ public class TCProfile extends VDLFunction {
 
 			attrs = attributesFromTC(tce, attrs);
 		}
-		attrs = addAttribute(attrs, "tr", tr);
 		addAttributes(named, attrs);
 		return null;
 	}
@@ -116,14 +115,6 @@ public class TCProfile extends VDLFunction {
 	        return;
 	    }
 	    named.add(GridExec.A_ATTRIBUTES, attrs);
-	}
-	
-	private Map addAttribute(Map attrs, String key, Object value) {
-	    if (attrs == null) {
-	        attrs = new HashMap();
-	    }
-	    attrs.put(key, value);
-	    return attrs;
 	}
 
 	private Map attributesFromTC(TransformationCatalogEntry tce, Map attrs) {
