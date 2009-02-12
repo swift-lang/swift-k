6b3a23b10fff2af36a745e6bd4acb4721608ba6a
Mihael Hategan <hategan@mcs.anl.gov>
2009-02-12 01:52:45 +0000

    compressed a bit; check if stuff returned by which exists and is executable; avoid messing with JAVA_HOME
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2291 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-coaster/resources/bootstrap.sh
+++ src/cog/modules/provider-coaster/resources/bootstrap.sh
@@ -1,10 +1,11 @@
-BS=$0
-LS=$1
-EMD5=$2
-LMD5=$3
-ID=$4
-H=$5
-L=$6
+BS=$1
+LS=$2
+EMD5=$3
+LMD5=$4
+ID=$5
+H=$6
+L=$7
+B="coaster-bootstrap"
 
 error() {
 	echo $1
@@ -16,11 +17,13 @@ find() {
 	R=`eval $1 2>/dev/null`
 	if [ "X$R" == "X" ]; then
 		R=`/bin/bash -l -c "$1"`
+	elif [ -x $R ]; then
+		R=`/bin/bash -l -c "$1"`
 	fi
 	echo $R
 }
 if [ "$L" == "" ]; then
-	L=~/coaster-boot-$ID.log 
+	L=~/$B-$ID.log 
 fi
 DJ=`mktemp /tmp/bootstrap.XXXXXX`
 echo "BS: $BS" >>$L
@@ -30,9 +33,9 @@ if [ "X$WGET" == "X" ]; then
 	if [ "X$WGET" == "X" ]; then
 		error "No wget or curl available"
 	fi
-	WGET="$WGET -O $DJ $BS/coaster-bootstrap.jar >>$L 2>&1"
+	WGET="$WGET -O $DJ $BS/$B.jar >>$L 2>&1"
 else
-	WGET="$WGET -c -q $BS/coaster-bootstrap.jar -O $DJ >>$L 2>&1"
+	WGET="$WGET -c -q $BS/$B.jar -O $DJ >>$L 2>&1"
 fi
 eval $WGET
 if [ "$?" != "0" ]; then
@@ -53,18 +56,15 @@ if [ "$AAMD5" != "$EMD5" ]; then
 	error "Bootstrap jar checksum failed: $EMD5 != $AAMD5"
 fi
 
-JAVA_HOME=`find 'echo $JAVA_HOME'`
-
-if [ "X$JAVA_HOME" == "X" ]; then
-	JAVA=`find 'which java'`
-	JAVA_HOME=$(dirname $JAVA)/..
-else 
+JAVA=`find 'which java'`
+if [ "X$JAVA" == "X" ]; then
 	JAVA=$JAVA_HOME/bin/java
 fi
 echo "JAVA=$JAVA" >>$L
 if [ -x $JAVA ]; then 
-	echo "$JAVA -Djava.home="$JAVA_HOME" -DX509_USER_PROXY="$X509_USER_PROXY" -DGLOBUS_HOSTNAME="$H" -jar $DJ $BS $LMD5 $LS $ID" >>$L
-	$JAVA -Djava.home="$JAVA_HOME" -DGLOBUS_TCP_PORT_RANGE="$GLOBUS_TCP_PORT_RANGE" -DX509_USER_PROXY="$X509_USER_PROXY" -DX509_CERT_DIR="$X509_CERT_DIR" -DGLOBUS_HOSTNAME="$H" -jar $DJ $BS $LMD5 $LS $ID
+	CMD="$JAVA -Djava=\"$JAVA\" -DGLOBUS_TCP_PORT_RANGE=\"$GLOBUS_TCP_PORT_RANGE\" -DX509_USER_PROXY=\"$X509_USER_PROXY\" -DX509_CERT_DIR=\"$X509_CERT_DIR\" -DGLOBUS_HOSTNAME=\"$H\" -jar $DJ $BS $LMD5 $LS $ID"
+	echo $CMD >>$L
+	eval $CMD >>$L	
 	EC=$?
 	echo "EC: $EC" >>$L
 	rm -f $DJ
