1c60b2fc2ce94ee581d7fb9827cf038d8a130801
Ben Clifford <benc@hawaga.org.uk>
2009-02-26 12:43:40 +0000

    make buildguides use the makefile
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2614 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- docs/buildguides.sh
+++ docs/buildguides.sh
@@ -1,36 +1,5 @@
 #!/bin/sh
 
-html() {
-	guide=$1
-	xsl=$2
-	out=${guide:0:${#guide}-4}.php
-	xsltproc formatting/$xsl $guide
-	#that's 'cause --output doesn't work
-	sed -e "s/index.html#/#/g" index.html >$out
-	rm index.html
-}
-
-pdf() {
-	guide=$1
-	xsl=$2
-	out=${guide:0:${#guide}-4}.pdf
-	fop/fop.sh -xsl formatting/vdl2_fo.xsl -xml $guide -pdf $out
-}
-
-process() {
-	html $1 $2
-	pdf $1
-}
-
-
-
-process "userguide.xml" "swiftsh_html.xsl"
-process "tutorial.xml" "swiftsh_html.xsl"
-process "tutorial-live.xml" "swiftsh_html.xsl"
-process "quickstartguide.xml" "swift_html.xsl"
-process "reallyquickstartguide.xml" "swift_html.xsl"
-process "languagespec.xml" "swift_html.xsl"
-process "languagespec-0.6.xml" "swift_html.xsl"
-process "log-processing.xml" "swift_html.xsl"
+make all chunked-userguide
 ./build-chunked-userguide.sh
 
