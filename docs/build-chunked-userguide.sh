1fa538350fa166a665e2abe199c7697ae9602388
Ben Clifford <benc@hawaga.org.uk>
2009-02-26 12:45:46 +0000

    chunked userguide needs JPEGs - this change was made on the deployed website some time ago but not committed
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2616 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- docs/build-chunked-userguide.sh
+++ docs/build-chunked-userguide.sh
@@ -4,6 +4,7 @@ mkdir -p userguide/ || exit 1
 cd userguide/ || exit 2
 rm -f *.html *.php
 cp ../*.png .
+cp ../*.jpeg .
 
 xsltproc --nonet ../formatting/swiftsh_html_chunked.xsl ../userguide.xml
 
