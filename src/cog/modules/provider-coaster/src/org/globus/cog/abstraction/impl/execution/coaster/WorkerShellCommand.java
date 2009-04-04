778ccf3b6169edaf8a12200da187e44270225796
Mihael Hategan <hategan@mcs.anl.gov>
2009-04-04 21:06:43 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2364 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/impl/execution/coaster/WorkerShellCommand.java
@@ -0,0 +1,22 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Feb 12, 2008
+ */
+package org.globus.cog.abstraction.impl.execution.coaster;
+
+import org.globus.cog.karajan.workflow.service.commands.Command;
+
+public class WorkerShellCommand extends Command {
+    public static final String NAME = "WORKERSHELLCMD";
+    
+    public WorkerShellCommand(String workerId, String command) {
+        super(NAME);
+        addOutData(workerId);
+        addOutData(command);
+    }
+}
