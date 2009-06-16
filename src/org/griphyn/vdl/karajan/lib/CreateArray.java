7f059e530f7b4883e57b10d7adcf8e7556a610f5
Ben Clifford <benc@hawaga.org.uk>
2009-06-16 16:06:23 +0000

    provenance information for explicitly constructed arrays made with vdl:createarray
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@2960 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/CreateArray.java
+++ src/org/griphyn/vdl/karajan/lib/CreateArray.java
@@ -3,6 +3,8 @@ package org.griphyn.vdl.karajan.lib;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.workflow.ExecutionException;
@@ -12,6 +14,9 @@ import org.griphyn.vdl.mapping.RootArrayDataNode;
 import org.griphyn.vdl.type.Type;
 
 public class CreateArray extends VDLFunction {
+
+	public static final Logger logger = Logger.getLogger(CreateArray.class);
+
 	public static final Arg PA_VALUE = new Arg.Positional("value");
 	
 	static {
@@ -54,20 +59,24 @@ public class CreateArray extends VDLFunction {
 
 			DSHandle handle = new RootArrayDataNode(type.arrayType());
 
+			logger.info("CREATEARRAY START array="+handle.getIdentifier());
+
 			int index = 0;
 			i = ((List) value).iterator();
 			while (i.hasNext()) {
 				// TODO check type consistency of elements with
 				// the type of the array
-				Object n = i.next();
-				Path p = Path.EMPTY_PATH.addLast(String.valueOf(index), true);
-
+				DSHandle n = (DSHandle) i.next();
 				// we know this DSHandle cast will work because we checked
 				// it in the previous scan of the array contents
-				handle.getField(p).set((DSHandle) n);
+				Path p = Path.EMPTY_PATH.addLast(String.valueOf(index), true);
+
+				handle.getField(p).set(n);
+				logger.info("CREATEARRAY MEMBER array="+handle.getIdentifier()+" index="+index+" member="+n.getIdentifier());
 				index++;
 			}
 			closeShallow(stack, handle);
+			logger.info("CREATEARRAY COMPLETED array="+handle.getIdentifier());
 
 			return handle;
 		}
