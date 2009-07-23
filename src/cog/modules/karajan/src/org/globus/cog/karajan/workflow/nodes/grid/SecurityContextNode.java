9a6acbdc68af09b186591cabe81cc5d3556b0e06
Mihael Hategan <hategan@mcs.anl.gov>
2009-07-23 20:12:29 +0000

    ability to specify a global credential through the api
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2443 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/grid/SecurityContextNode.java
+++ src/cog/modules/karajan/src/org/globus/cog/karajan/workflow/nodes/grid/SecurityContextNode.java
@@ -20,21 +20,25 @@ import org.globus.cog.karajan.workflow.nodes.functions.AbstractFunction;
 public class SecurityContextNode extends AbstractFunction {
 	public static final Arg A_PROVIDER = new Arg.Positional("provider");
 	public static final Arg A_CREDENTIALS = new Arg.Positional("credentials");
-	
+
 	static {
 		setArguments(SecurityContextNode.class, new Arg[] { A_PROVIDER, A_CREDENTIALS });
 	}
 
 	public Object function(VariableStack stack) throws ExecutionException {
-		String provider = TypeUtil.toString(A_PROVIDER.getValue(stack));
-		SecurityContext context;
-		try {
-			context = AbstractionFactory.newSecurityContext(provider);
+		SecurityContext context = (SecurityContext) checkClass(
+				stack.getExecutionContext().getAttribute("securityContext"), SecurityContext.class,
+				"SecurityContext");
+		if (context == null) {
+		    String provider = TypeUtil.toString(A_PROVIDER.getValue(stack));
+			try {
+				context = AbstractionFactory.newSecurityContext(provider);
+			}
+			catch (Exception e) {
+				throw new ExecutionException("Unsupported security context type: " + provider, e);
+			}
+			context.setCredentials(A_CREDENTIALS.getValue(stack));
 		}
-		catch (Exception e) {
-			throw new ExecutionException("Unsupported security context type: " + provider, e);
-		}
-		context.setCredentials(A_CREDENTIALS.getValue(stack));
 		return context;
 	}
 }
