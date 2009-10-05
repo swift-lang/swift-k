35ccb9b2ff81d2aa4dc4c7bbb58d9e73f0976f41
Mihael Hategan <hategan@mcs.anl.gov>
2009-10-05 19:11:03 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2463 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ src/cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/channels/SendCallback.java
@@ -0,0 +1,14 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+/*
+ * Created on Oct 3, 2009
+ */
+package org.globus.cog.karajan.workflow.service.channels;
+
+public interface SendCallback {
+	void dataSent();
+}
