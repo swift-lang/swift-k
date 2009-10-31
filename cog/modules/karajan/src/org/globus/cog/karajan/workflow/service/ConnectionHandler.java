3f22826c2c3736dea4f827c5baaa5925c91a1f86
Mihael Hategan <hategan@mcs.anl.gov>
2009-10-31 00:15:41 +0000

    constructor for a stream-only handler
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@2558 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/ConnectionHandler.java
+++ cog/modules/karajan/src/org/globus/cog/karajan/workflow/service/ConnectionHandler.java
@@ -10,20 +10,23 @@
 package org.globus.cog.karajan.workflow.service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import org.apache.log4j.Logger;
-import org.globus.cog.karajan.workflow.service.channels.AbstractTCPChannel;
 import org.globus.cog.karajan.workflow.service.channels.ChannelContext;
 import org.globus.cog.karajan.workflow.service.channels.GSSChannel;
+import org.globus.cog.karajan.workflow.service.channels.KarajanChannel;
+import org.globus.cog.karajan.workflow.service.channels.StreamChannel;
 import org.globus.cog.karajan.workflow.service.channels.TCPChannel;
 import org.globus.gsi.gssapi.net.GssSocket;
 
 public class ConnectionHandler {
 	private static final Logger logger = Logger.getLogger(ConnectionHandler.class);
 
-	private final Socket socket;
-	private final AbstractTCPChannel channel;
+	private Socket socket;
+	private final KarajanChannel channel;
 	private final RequestManager requestManager;
 
 	public ConnectionHandler(Service service, Socket socket) throws IOException {
@@ -31,8 +34,9 @@ public class ConnectionHandler {
 	}
 
 	public ConnectionHandler(Service service, Socket socket, RequestManager requestManager) throws IOException {
+	    this.requestManager = requestManager == null ? new ServiceRequestManager() : requestManager;
 		this.socket = socket;
-		this.requestManager = requestManager == null ? new ServiceRequestManager() : requestManager;
+		
 		if (socket instanceof GssSocket) {
 			channel = new GSSChannel((GssSocket) socket, this.requestManager, new ChannelContext(
 					service));
@@ -41,10 +45,17 @@ public class ConnectionHandler {
 			channel = new TCPChannel(socket, this.requestManager, new ChannelContext(service));
 		}
 	}
+	
+	public ConnectionHandler(Service service, InputStream is, OutputStream os, RequestManager requestManager) {
+	    this.requestManager = requestManager == null ? new ServiceRequestManager() : requestManager;
+	    channel = new StreamChannel(is, os, this.requestManager, new ChannelContext(service));
+	}
 
 	public void start() throws Exception {
-		socket.setKeepAlive(true);
-		socket.setSoTimeout(0);
+	    if (socket != null) {
+	    	socket.setKeepAlive(true);
+	    	socket.setSoTimeout(0);
+	    }
 		channel.start();
     }
 }
