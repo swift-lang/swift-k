7ad619137b2ab77ed74475ff85d41bb7ab6cc46d
Mihael Hategan <hategan@mcs.anl.gov>
2009-11-06 17:14:44 +0000

    null looks ugly if prefix is not specified
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3190 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/mapping/file/ConcurrentMapper.java
+++ src/org/griphyn/vdl/mapping/file/ConcurrentMapper.java
@@ -24,7 +24,8 @@ public class ConcurrentMapper extends AbstractFileMapper {
 
 	public void setParams(Map params) {
 		String prefix = PARAM_PREFIX.getStringValue(this);
-		prefix = "_concurrent/" + prefix + "-" + PARAM_THREAD_PREFIX.getValue(params);
+		prefix = "_concurrent/" + (prefix == null ? "" : prefix + "-") + 
+		    PARAM_THREAD_PREFIX.getValue(params);
 		PARAM_PREFIX.setValue(params, prefix);
 		super.setParams(params);
 	}
