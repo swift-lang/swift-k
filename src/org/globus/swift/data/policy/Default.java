3225246c72ac72290d72cbeebfdb9b6b6e5733a8
Justin Wozniak <wozniak@mcs.anl.gov>
2010-02-10 18:34:11 +0000

    Added abstract method.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3236 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/globus/swift/data/policy/Default.java
+++ src/org/globus/swift/data/policy/Default.java
@@ -1,9 +1,15 @@
 
 package org.globus.swift.data.policy;
 
+import java.util.List;
+
 public class Default extends Policy {
 
     public String toString() {
         return "DEFAULT";
+    }
+
+    @Override
+    public void settings(List<String> tokens) {
     }
 }
