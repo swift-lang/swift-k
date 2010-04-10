eae482dfede0d6a9b4a7ea7e5a8c0c0f014e473a
Justin Wozniak <wozniak@mcs.anl.gov>
2010-04-10 19:16:56 +0000

    Corrections to CDM BROADCAST for BG/P.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3277 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- libexec/cdm_broadcast.sh
+++ libexec/cdm_broadcast.sh
@@ -2,13 +2,53 @@
 
 SWIFT_HOME=$( dirname $( dirname $0 ) )
 LOG=${SWIFT_HOME}/etc/cdm_broadcast.log
+
+bgp_broadcast()
 {
+  DIR=$1
+  FILE=$2
+  DEST=$3
+  if [[ ! -f ips.list ]] 
+    then
+    BLOCKS=$( qstat -u ${USER} | grep ${USER} | awk '{ print $6 }' )
+    IPS=$( listip ${BLOCKS} )
+    for IP in ${IPS}
+     do
+     echo ${IP}
+    done >> ip.list
+  else
+    while read T
+     do 
+     BLOCKS="$BLOCKS $T" 
+    done < ip.list
+  fi
+  for IP in ${BLOCKS}
+   do
+   ssh ${IP} /bin.rd/f2cn ${DIR}/${FILE} ${DEST}/${FILE}
+  done
+}
+
+local_broadcast()
+{
+  DIR=$1
+  FILE=$2
+  DEST=$3
+  cp -v ${FILE} ${DEST}/${FILE}
+}
+  
+{
+  declare -p PWD
     set -x
 
     FILE=$1
     DIR=$2
     DEST=$3
-    
-    cp -v ${DIR}/${FILE} ${DEST}
+
+    if [[ $( uname -p ) == "ppc64" ]]
+      then
+      bgp_broadcast ${DIR} ${FILE} ${DEST}
+    else 
+      bgp_local ${DIR} ${FILE} ${DEST}
+    fi
 
 } >> ${LOG} 2>&1
