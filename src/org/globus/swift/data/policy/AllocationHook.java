a4f50a59a1adfba4a9635539c77c9c70db3c18f2
Justin Wozniak <wozniak@mcs.anl.gov>
2010-04-11 14:14:59 +0000

    Draft of hook for Coasters integration with CDM.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3278 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ src/org/globus/swift/data/policy/AllocationHook.java
@@ -0,0 +1,12 @@
+
+import org.globus.cog.abstraction.interfaces.Status;
+import org.globus.cog.abstraction.impl.common.StatusEvent;
+import org.globus.cog.abstraction.coaster.service.job.manager.Hook;
+
+public class AllocationHook extends Hook
+{
+    public void blockActive(StatusEvent e)
+    {
+        System.out.println("blockActive: " + e.getStatus().getMessage());
+    }
+}
