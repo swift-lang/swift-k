651dc384780fa23deeb1920a1682f4c867f37ea5
Justin Wozniak <wozniak@mcs.anl.gov>
2010-06-04 18:34:13 +0000

    Added the ability to accept input on the socket
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2754 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/job/manager/TCPTest.java
+++ src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/job/manager/TCPTest.java
@@ -9,18 +9,44 @@
  */
 package org.globus.cog.abstraction.coaster.service.job.manager;
 
-import java.net.ServerSocket;
+import java.io.*;
+import java.net.*;
 
 public class TCPTest {
     public static void main(String[] args) {
         try {
-            final ServerSocket ss = new ServerSocket(40000);
-            new Thread() {
-                
-            }.start();
+          ServerSocket ss = new ServerSocket(40000);
+          System.out.println(ss);
+          while (true)
+          {
+            Socket socket = ss.accept();
+            process(socket);
+            socket.close();
+          }
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    static void process(Socket socket)
+        throws Exception
+    {
+        System.out.println("connected");
+        while (true)
+        {
+            BufferedReader reader =
+                new BufferedReader
+                (new InputStreamReader(socket.getInputStream()));
+            OutputStreamWriter writer =
+                new OutputStreamWriter(socket.getOutputStream());
+
+            String line = reader.readLine();
+            System.out.println("\t" + line);
+            if (line == null)
+                break;
+            if (line.equals("quit"))
+                break;
         }
     }
 }
