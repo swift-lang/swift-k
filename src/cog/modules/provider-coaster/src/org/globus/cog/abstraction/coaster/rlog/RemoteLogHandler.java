3cd5978a5320dd261b9d12e301799119898026cd
Mihael Hategan <hategan@mcs.anl.gov>
2010-07-01 03:59:17 +0000

    updated to include a type
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2781 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/rlog/RemoteLogHandler.java
+++ src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/rlog/RemoteLogHandler.java
@@ -16,8 +16,33 @@ import org.globus.cog.karajan.workflow.service.handlers.RequestHandler;
 public class RemoteLogHandler extends RequestHandler {
     public static final Logger logger = Logger.getLogger(RemoteLogHandler.class);
 
+    @Override
     public void requestComplete() throws ProtocolException {
-        logger.info(getInDataAsString(0));
+        RemoteLogCommand.Type type = RemoteLogCommand.Type.valueOf(getInDataAsString(0));
+        String msg = getInDataAsString(1);
+        switch (type) {
+            case STDOUT:
+                System.out.println(msg);
+                break;
+            case STDERR:
+                System.err.println(msg);
+                break;
+            case FATAL:
+                logger.fatal(msg);
+                break;
+            case ERROR:
+                logger.error(msg);
+                break;
+            case WARN:
+                logger.warn(msg);
+                break;
+            case INFO:
+                logger.info(msg);
+                break;
+            case DEBUG:
+                logger.debug(msg);
+                break;
+        }
         sendReply("OK");
     }
 }
