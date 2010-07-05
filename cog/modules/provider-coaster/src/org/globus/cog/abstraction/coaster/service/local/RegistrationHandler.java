244ebb314c92c0a89d4daa4ce61be0a733f4d462
Justin Wozniak <wozniak@mcs.anl.gov>
2010-07-05 17:28:08 +0000

    Readability
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@2795 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/local/RegistrationHandler.java
+++ cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/local/RegistrationHandler.java
@@ -14,20 +14,28 @@ import java.net.URI;
 import org.globus.cog.abstraction.coaster.service.Registering;
 import org.globus.cog.karajan.workflow.service.ProtocolException;
 import org.globus.cog.karajan.workflow.service.channels.AbstractStreamKarajanChannel;
+import org.globus.cog.karajan.workflow.service.channels.ChannelContext;
+import org.globus.cog.karajan.workflow.service.channels.KarajanChannel;
 import org.globus.cog.karajan.workflow.service.handlers.RequestHandler;
 
 public class RegistrationHandler extends RequestHandler {
     public static final String NAME = "REGISTER";
 
+    @Override
     public void requestComplete() throws ProtocolException {
         String id = this.getInDataAsString(0);
         String url = this.getInDataAsString(1);
-        Registering ls = (Registering) getChannel().getChannelContext()
-                .getService();
+        KarajanChannel channel = getChannel();
+        ChannelContext context = channel.getChannelContext();
+        Registering ls = (Registering) context.getService();
         try {
-            String rid = ls.registrationReceived(id, url, getChannel());
-            if (getChannel() instanceof AbstractStreamKarajanChannel) {
-            	((AbstractStreamKarajanChannel) getChannel()).setContact(new URI(id + (rid == null ? "" : "-" + rid)));
+            String rid = ls.registrationReceived(id, url, channel);
+            if (channel instanceof AbstractStreamKarajanChannel) {
+                AbstractStreamKarajanChannel askc =
+                    (AbstractStreamKarajanChannel) channel;
+                String s = id + (rid == null ? "" : "-" + rid);
+                URI uri = new URI(s);
+            	askc.setContact(uri);
             }
             this.sendReply(rid == null ? "OK" : rid);
         }
