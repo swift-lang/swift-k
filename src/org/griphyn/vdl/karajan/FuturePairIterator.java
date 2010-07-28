fc9a13b3c395294b0f7a7ce9ad2892a0883e334e
Mihael Hategan <hategan@mcs.anl.gov>
2010-07-28 19:36:27 +0000

    no need to chain iterator future notifications in this case since they can be added directly to the array
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3485 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/FuturePairIterator.java
+++ src/org/griphyn/vdl/karajan/FuturePairIterator.java
@@ -3,37 +3,25 @@
  */
 package org.griphyn.vdl.karajan;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-
 import org.globus.cog.karajan.stack.VariableNotFoundException;
 import org.globus.cog.karajan.stack.VariableStack;
-import org.globus.cog.karajan.workflow.ExecutionException;
-import org.globus.cog.karajan.workflow.events.ControlEvent;
-import org.globus.cog.karajan.workflow.events.ControlEventType;
 import org.globus.cog.karajan.workflow.events.Event;
-import org.globus.cog.karajan.workflow.events.EventBus;
 import org.globus.cog.karajan.workflow.events.EventListener;
-import org.globus.cog.karajan.workflow.events.EventTargetPair;
-import org.globus.cog.karajan.workflow.futures.Future;
 import org.globus.cog.karajan.workflow.futures.FutureEvaluationException;
 import org.globus.cog.karajan.workflow.futures.FutureIterator;
 import org.globus.cog.karajan.workflow.futures.FutureIteratorIncomplete;
 import org.globus.cog.karajan.workflow.futures.FutureNotYetAvailable;
 
-public class FuturePairIterator implements FutureIterator, EventListener, Mergeable {
+public class FuturePairIterator implements FutureIterator {
 	private ArrayIndexFutureList array;
 	private int crt;
-	private LinkedList listeners;
 
 	public FuturePairIterator(ArrayIndexFutureList array) {
 		this.array = array;
-		array.addModificationAction(this, null);
 	}
 
 	public FuturePairIterator(ArrayIndexFutureList array, VariableStack stack) {
 		this.array = array;
-		array.addModificationAction(this, new ControlEvent(null, ControlEventType.RESTART, stack));
 	}
 
 	public synchronized boolean hasAvailable() {
@@ -107,43 +95,11 @@ public class FuturePairIterator implements FutureIterator, EventListener, Mergea
 	}
 
 	public synchronized void addModificationAction(EventListener target, Event event) {
-		if (listeners == null) {
-			listeners = new LinkedList();
-		}
 		WaitingThreadsMonitor.addThread(event.getStack());
-		listeners.add(new EventTargetPair(event, target));
+		array.addModificationAction(target, event);
 	}
-
-	private void notifyListeners() {
-		synchronized (this) {
-			if (listeners == null) {
-				return;
-			}
-
-			Iterator i = listeners.iterator();
-			while (i.hasNext()) {
-				EventTargetPair etp = (EventTargetPair) i.next();
-				WaitingThreadsMonitor.removeThread(etp.getEvent().getStack());
-				EventBus.post(etp.getTarget(), etp.getEvent());
-				i.remove();
-			}
-			
-			listeners = null;
-		}
-	}
-
-	public void event(Event e) throws ExecutionException {
-		notifyListeners();
-	}
-
-	public void mergeListeners(Future f) {
-		Iterator i = listeners.iterator();
-		while (i.hasNext()) {
-			EventTargetPair etp = (EventTargetPair) i.next();
-			f.addModificationAction(etp.getTarget(), etp.getEvent());
-			i.remove();
-		}
-	}
+	
+	private static volatile int cnt = 0;
 
 	public void fail(FutureEvaluationException e) {
 		//handled by the list
