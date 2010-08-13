47ab9e937ed1b04155663776bd9dba724045dc8a
Mihael Hategan <hategan@mcs.anl.gov>
2010-08-13 19:15:42 +0000

    removed direct execution
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2850 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/karajan/src/org/globus/cog/karajan/workflow/nodes/functions/NumericValue.java
+++ modules/karajan/src/org/globus/cog/karajan/workflow/nodes/functions/NumericValue.java
@@ -12,10 +12,9 @@ package org.globus.cog.karajan.workflow.nodes.functions;
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.util.TypeUtil;
-import org.globus.cog.karajan.workflow.DirectExecution;
 import org.globus.cog.karajan.workflow.ExecutionException;
 
-public class NumericValue extends AbstractFunction implements DirectExecution {
+public class NumericValue extends AbstractFunction {
 	public static final Arg A_VALUE = new Arg.Positional("value");
 
 	static {
