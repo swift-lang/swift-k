358f4ea0fe02fa78303f7a0bce4b017b33b3521c
Mihael Hategan <hategan@mcs.anl.gov>
2010-08-14 02:52:25 +0000

    added fake provider
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2857 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ src/cog/modules/provider-fake/src/org/globus/cog/abstraction/impl/execution/fake/JobSubmissionTaskHandler.java
@@ -0,0 +1,79 @@
+//----------------------------------------------------------------------
+//This code is developed as part of the Java CoG Kit project
+//The terms of the license can be found at http://www.cogkit.org/license
+//This message may not be removed or altered.
+//----------------------------------------------------------------------
+
+package org.globus.cog.abstraction.impl.execution.fake;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+import org.globus.cog.abstraction.impl.common.AbstractDelegatedTaskHandler;
+import org.globus.cog.abstraction.impl.common.StatusImpl;
+import org.globus.cog.abstraction.impl.common.task.IllegalSpecException;
+import org.globus.cog.abstraction.impl.common.task.InvalidSecurityContextException;
+import org.globus.cog.abstraction.impl.common.task.InvalidServiceContactException;
+import org.globus.cog.abstraction.impl.common.task.TaskSubmissionException;
+import org.globus.cog.abstraction.interfaces.JobSpecification;
+import org.globus.cog.abstraction.interfaces.Status;
+import org.globus.cog.abstraction.interfaces.Task;
+
+public class JobSubmissionTaskHandler extends AbstractDelegatedTaskHandler {
+    private static Logger logger = Logger.getLogger(JobSubmissionTaskHandler.class);
+
+
+    public static final Timer TIMER = new Timer();
+    
+    public void submit(final Task task) throws IllegalSpecException, InvalidSecurityContextException,
+            InvalidServiceContactException, TaskSubmissionException {
+        checkAndSetTask(task);
+        task.setStatus(Status.SUBMITTING);
+        JobSpecification spec;
+        try {
+            spec = (JobSpecification) task.getSpecification();
+        }
+        catch (Exception e) {
+            throw new IllegalSpecException("Exception while retrieving Job Specification", e);
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug(spec.toString());
+        }
+
+        try {
+            int delay = 0;
+            synchronized (this) {
+                if (task.getStatus().getStatusCode() != Status.CANCELED) {
+                    task.setStatus(Status.SUBMITTED);
+                    if (spec.isBatchJob()) {
+                        task.setStatus(Status.COMPLETED);
+                    }
+                    else {
+                        TIMER.schedule(new TimerTask() {
+                        public void run() {
+                            task.setStatus(Status.COMPLETED);
+                        }}, delay * 1000);
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new TaskSubmissionException("Cannot submit job", e);
+        }
+    }
+
+    public void suspend() throws InvalidSecurityContextException, TaskSubmissionException {
+    }
+
+    public void resume() throws InvalidSecurityContextException, TaskSubmissionException {
+    }
+
+    public void cancel(String message) throws InvalidSecurityContextException,
+            TaskSubmissionException {
+        synchronized (this) {
+            getTask().setStatus(new StatusImpl(Status.CANCELED, message, null));
+        }
+    }
+}
