43c01013cf1d7c59f0d855286ce3380969c98c8e
Justin Wozniak <wozniak@mcs.anl.gov>
2010-08-27 18:42:41 +0000

    Skip stagein in provider staging for CDM files
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3571 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/AppStageins.java
+++ src/org/griphyn/vdl/karajan/lib/AppStageins.java
@@ -6,15 +6,20 @@ package org.griphyn.vdl.karajan.lib;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.arguments.ArgUtil;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.util.TypeUtil;
 import org.globus.cog.karajan.workflow.ExecutionException;
 import org.globus.cog.karajan.workflow.nodes.AbstractSequentialWithArguments;
+import org.globus.swift.data.Director;
+import org.globus.swift.data.policy.Policy;
 import org.griphyn.vdl.mapping.AbsFile;
 
 public class AppStageins extends AbstractSequentialWithArguments {
+
+    static Logger logger = Logger.getLogger(AppStageins.class);
 
     public static final Arg JOBID = new Arg.Positional("jobid");
     public static final Arg FILES = new Arg.Positional("files");
@@ -31,16 +36,24 @@ public class AppStageins extends AbstractSequentialWithArguments {
         List files = TypeUtil.toList(FILES.getValue(stack));
         for (Object f : files) {
             AbsFile file = new AbsFile(TypeUtil.toString(f));
+            Policy policy = Director.lookup(file.toString());
+            if (policy != Policy.DEFAULT) {
+                logger.debug("will not stage in (CDM): " + file);
+                continue; 
+            }
+                                        
             String protocol = file.getProtocol();
             if (protocol.equals("file")) {
                 protocol = TypeUtil.toString(STAGING_METHOD.getValue(stack));
             }
-            String path = file.getDir().equals("") ? file.getName() : file.getDir()
+            String path = file.getDir().equals("") ? file.getName() : file
+                .getDir()
                     + "/" + file.getName();
             String relpath = path.startsWith("/") ? path.substring(1) : path;
+            logger.debug("will stage in: " + relpath + " via: " + protocol);
             ArgUtil.getChannelReturn(stack, STAGEIN).append(
-                makeList(protocol + "://" + file.getHost() + "/" + path, TypeUtil.toString(DIR.getValue(stack))
-                        + "/" + relpath));
+                makeList(protocol + "://" + file.getHost() + "/" + path,
+                    TypeUtil.toString(DIR.getValue(stack)) + "/" + relpath));
         }
         super.post(stack);
     }
