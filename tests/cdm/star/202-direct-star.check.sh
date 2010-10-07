dc4d3d24fe47a95b67318fe0d6851349e21dfe27
Justin Wozniak <wozniak@mcs.anl.gov>
2010-10-07 17:28:57 +0000

    Test for CDM with simple .* pattern
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3661 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/cdm/star/202-direct-star.check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -x
+
+grep $( uname -m ) 201/201-input.txt  || exit 1
+grep $( uname -m ) 201/201-output.txt || exit 1
+
+exit 0
---
+++ tests/cdm/star/202-direct-star.clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -x
+
+rm -rv 201 || exit 1
+
+exit 0
