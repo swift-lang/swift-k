3ce1823c29c43c7086137424690e8d494564ea5a
Sarah Kenny <skenny@uchicago.edu>
2010-10-26 21:59:31 +0000

    code for reallyquickstartguide now saved, commented out, in the quickstartguide
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3698 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- docs/guides/quickstartguide.php
+++ docs/guides/quickstartguide.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Swift Quick Start Guide</title><meta name="generator" content="DocBook XSL Stylesheets V1.75.2"><meta name="description" content="The impatient may find the Swift Really Quick Start Guide to be more convenient. This guide describes the steps needed to download, install, configure, and run the basic examples for Swift. If you are using a pre-installed version of Swift, you can skip directly to the configuration section."><link rel="home" href="index.html" title="Swift Quick Start Guide"><link href="http://www.ci.uchicago.edu/swift/css/style1col.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/dhtml.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shCoreu.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shBrushVDL2.js"></script></head><body onLoad="initjs();sh();" class="section-3">
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Swift Quick Start Guide</title><meta name="generator" content="DocBook XSL Stylesheets V1.75.2"><meta name="description" content="This guide describes the steps needed to download, install, configure, and run the basic examples for Swift. If you are using a pre-installed version of Swift, you can skip directly to the configuration section."><link rel="home" href="index.html" title="Swift Quick Start Guide"><link href="http://www.ci.uchicago.edu/swift/css/style1col.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/dhtml.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shCoreu.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shBrushVDL2.js"></script></head><body onLoad="initjs();sh();" class="section-3">
 		
 		<!-- entire page container -->
 		<div id="container">
@@ -18,16 +18,13 @@
 			<!-- content container -->
 			<div id="content">
 		
-		<div class="article" title="Swift Quick Start Guide"><div class="titlepage"><div><div><h2 class="title"><a name="id2601088"></a>Swift Quick Start Guide</h2></div><div><h3 class="subtitle"><i>Source Control $LastChangedRevision: 2810 $</i></h3></div><div><div class="abstract" title="Abstract"><p class="title"><b>Abstract</b></p><p><b> </b>
+		<div class="article" title="Swift Quick Start Guide"><div class="titlepage"><div><div><h2 class="title"><a name="id2601088"></a>Swift Quick start Guide</h2></div><div><h3 class="subtitle"><i>Source Control $LastChangedRevision: 2810 $</i></h3></div><div><div class="abstract" title="Abstract"><p class="title"><b>Abstract</b></p><p><b> </b>
 				
-					The impatient may find the <a class="ulink" href="reallyquickstartguide.php" target="_top">Swift Really Quick Start
-					Guide</a> to be more convenient.
-
-					This guide describes the steps needed to download, install,
-					configure, and run the basic examples for Swift. If you are
-					using a pre-installed version of Swift, you can skip 
-					directly to the <a class="link" href="#configure" title="3. Configuring Swift">configuration
-					section</a>.
+				This guide describes the steps needed to download, install,
+				configure, and run the basic examples for Swift. If you are
+				using a pre-installed version of Swift, you can skip 
+				directly to the <a class="link" href="#configure" title="3. Configuring Swift">configuration
+				section</a>.
 
 				</p></div></div></div><hr></div><div class="toc"><p><b>Table of Contents</b></p><dl><dt><span class="sect1"><a href="#download">1. Downloading a Swift Distribution</a></span></dt><dd><dl><dt><span class="sect2"><a href="#dl-stable">1.1. Stable Releases</a></span></dt><dt><span class="sect2"><a href="#dl-nightly">1.2. Nightly Builds</a></span></dt><dt><span class="sect2"><a href="#dl-repository">1.3. Source Repository</a></span></dt></dl></dd><dt><span class="sect1"><a href="#install">2. Installing a Swift Binary Package</a></span></dt><dt><span class="sect1"><a href="#configure">3. Configuring Swift</a></span></dt><dd><dl><dt><span class="sect2"><a href="#security">3.1. Grid Security</a></span></dt><dt><span class="sect2"><a href="#id2668393">3.2. Swift Properties</a></span></dt></dl></dd><dt><span class="sect1"><a href="#examples">4. Running Swift Examples</a></span></dt></dl></div><div class="sect1" title="1. Downloading a Swift Distribution"><div class="titlepage"><div><div><h2 class="title"><a name="download"></a>1. Downloading a Swift Distribution</h2></div></div></div><p>
 		
@@ -190,3 +187,97 @@ try {
 var pageTracker = _gat._getTracker("UA-106257-5");
 pageTracker._trackPageview();
 } catch(err) {}</script></body></html>
+
+<!--
+ <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Swift Really Quick Start Guide</title><meta name="generator" content="DocBook XSL Stylesheets V1.75.2"><meta name="description" content="This guide is a compressed version of the Swift Quick Start Guide."><link rel="home" href="index.html" title="Swift Really Quick Start Guide"><link href="http://www.ci.uchicago.edu/swift/css/style1col.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/dhtml.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shCoreu.js"></script><script type="text/javascript" src="http://www.ci.uchicago.edu/swift/shBrushVDL2.js"></script></head><body onLoad="initjs();sh();" class="section-3">
+		
+		<!-- entire page container -->
+		<div id="container">
+			<!-- header -->
+			<div id="header">
+				<?php require('/disks/space0/projects/swift/inc/header.php') ?>
+				<?php #require('/ci/www/projects/swift/inc/header.php') ?>
+			</div>
+			<!-- end header -->
+			<!-- nav -->
+			<div id="nav">
+				<?php require('/disks/space0/projects/swift/inc/nav.php') ?>
+				<?php #require('/ci/www/projects/swift/inc/nav.php') ?>
+			</div>
+			<!-- end nav -->
+			<!-- content container -->
+			<div id="content">
+		
+		<div class="article" title="Swift Really Quick Start Guide"><div class="titlepage"><div><div><h2 class="title"><a name="id2725197"></a>Swift Really Quick Start Guide</h2></div><div><h3 class="subtitle"><i>Source control $LastChangedRevision: 2646 $</i></h3></div><div><div class="abstract" title="Abstract"><p class="title"><b>Abstract</b></p><p><b> </b>
+				
+					This guide is a compressed version of the <a class="ulink" href="quickstartguide.php" target="_top">Swift Quick Start
+					Guide</a>.
+				
+				</p></div></div></div><hr></div><div class="toc"><p><b>Table of Contents</b></p><dl><dt><span class="sect1"><a href="#reallyquickstart">1. Swift Really Quick Start Guide</a></span></dt></dl></div><div class="sect1" title="1. Swift Really Quick Start Guide"><div class="titlepage"><div><div><h2 class="title"><a name="reallyquickstart"></a>1. Swift Really Quick Start Guide</h2></div></div></div><div class="itemizedlist"><ul class="itemizedlist" type="disc"><li class="listitem"><p>
+				
+					<a class="ulink" href="http://www.ci.uchicago.edu/swift/downloads/index.php" target="_top">Download</a>
+					Swift.
+				
+				</p></li><li class="listitem"><p>
+				
+					Unpack it and add the <code class="filename">swift-xyz/bin</code> directory to your
+					<code class="envar">PATH</code>.
+				
+				</p></li><li class="listitem"><p>
+				
+					Make sure you have your user certificate, a valid GSI proxy
+					certificate, and the proper CA root certificates in either
+					<code class="filename">~/.globus/certificates</code> or
+					<code class="filename">/etc/grid-security/certificates</code>.
+				
+				</p></li><li class="listitem"><p>
+				
+					Edit <code class="filename">swift-xyz/etc/swift.properties</code>. You
+					should add your numeric IP address there
+					(<span class="property">ip.address</span>=<code class="literal">x.y.z.w</code>).
+				
+				</p></li><li class="listitem"><p>
+				
+					Use the example site catalog and transformation catalog (they 
+					are configured for local submission):
+					
+</p><pre class="screen">
+<span class="command"><strong>cd</strong></span> swift-xyz/etc
+<span class="command"><strong>cp</strong></span> sites.xml.example sites.xml
+<span class="command"><strong>cp</strong></span> tc.data.example tc.data
+</pre><p>
+				
+				</p></li><li class="listitem"><p>
+				
+					Use <span class="command"><strong>swift file.dtm</strong></span> to compile and execute
+					<code class="filename">file.dtm</code>.
+				
+				</p></li><li class="listitem"><p>
+				
+					Use <span class="command"><strong>swift -resume file-&lt;runid&gt;.?.rlog
+					file.dtm</strong></span> to resume a failed run.
+				
+				</p></li></ul></div></div></div>
+			</div>
+			<!-- end content container-->
+			<!-- footer -->
+			<div id="footer">
+				<?php require('/disks/space0/projects/swift/inc/footer.php') ?>
+				<?php #require('/ci/www/projects/swift/inc/footer.php') ?>
+			</div> 
+			<!-- end footer -->
+
+		</div>
+		<!-- end entire page container -->
+
+		
+		<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-106257-5");
+pageTracker._trackPageview();
+} catch(err) {}</script></body></html>
+-->
