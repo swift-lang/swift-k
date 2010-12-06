c6f53c994b6d7ef45a657894b553edccb88ace5f
Justin Wozniak <wozniak@mcs.anl.gov>
2010-12-06 21:28:00 +0000

    Fix comment
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3740 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/cdm/301-bcast.swift
+++ tests/cdm/301-bcast.swift
@@ -6,7 +6,7 @@ app (file o) copy (file i)
   cp @i @o;
 }
 
-// Will be transformed by CDM to 201/output.txt:
+// Will be transformed by CDM to 301/301-input.txt:
 file f1<"301-input.txt">;
 
 file f2<"301-output.txt">;
