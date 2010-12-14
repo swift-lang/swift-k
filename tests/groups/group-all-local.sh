c43555b1a30e07b47225d3744e6a183417cd8b43
Justin Wozniak <wozniak@mcs.anl.gov>
2010-12-14 21:59:07 +0000

    First example GROUPLIST defn files
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@3802 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/groups/group-all-local.sh
@@ -0,0 +1,13 @@
+
+# GROUPLIST definition to run all local tests
+
+GROUPLIST=( $TESTDIR/language-behaviour \
+            $TESTDIR/language/working \
+            $TESTDIR/local \
+            $TESTDIR/language/should-not-work \
+            $TESTDIR/cdm \
+            $TESTDIR/cdm/ps \
+            $TESTDIR/cdm/star
+            $TESTDIR/cdm/ps/pinned
+	    # $TESTDIR/site/intrepid
+          )
---
+++ tests/groups/group-intrepid.sh
@@ -0,0 +1,6 @@
+
+# GROUPLIST definition to run on Intrepid
+
+GROUPLIST=( $TESTDIR/local \
+	    $TESTDIR/site/intrepid
+          )
