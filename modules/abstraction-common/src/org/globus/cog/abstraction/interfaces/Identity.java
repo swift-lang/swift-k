1218ee386f7726305e2b2e0ad5c9979bce5a62c3
Justin Wozniak <wozniak@mcs.anl.gov>
2010-12-23 17:06:13 +0000

    Spelling
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@2982 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/abstraction-common/src/org/globus/cog/abstraction/interfaces/Identity.java
+++ modules/abstraction-common/src/org/globus/cog/abstraction/interfaces/Identity.java
@@ -9,7 +9,7 @@ package org.globus.cog.abstraction.interfaces;
 /**
  * A representation of a unique identity held by various cog-core objects. An
  * <code>Identity</code> is a {@link URI}, collectively composed of a
- * <code>namespece</code> and <code>value</code>. A sample
+ * <code>namespace</code> and <code>value</code>. A sample
  * <code>Identity</code> looks as follows:
  * <p>
  * urn:&lt;namespace&gt;:&lt;value&gt;
