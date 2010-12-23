63d6c0b0793def2665e020361e5cdf8ba111b890
Justin Wozniak <wozniak@mcs.anl.gov>
2010-12-23 17:03:31 +0000

    Add logging
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@2979 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/util/src/org/globus/cog/util/StreamProcessor.java
+++ src/cog/modules/util/src/org/globus/cog/util/StreamProcessor.java
@@ -8,50 +8,56 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import org.apache.log4j.Logger;
+
 /**
  * Utility class to read a stream and report when a pattern is found
  * @author wozniak
  */
 public class StreamProcessor extends Streamer {
 
+    public static final Logger logger = Logger.getLogger(StreamProcessor.class);
+
     /**
        Object to notify when pattern is found
      */
     Object object;
-    
+
     String pattern;
-    
+
     boolean matched = false;
-    
-    public StreamProcessor(InputStream istream, OutputStream ostream, 
-                           Object object, String pattern) { 
+
+    public StreamProcessor(InputStream istream, OutputStream ostream,
+                           Object object, String pattern) {
         super(istream, ostream);
         this.object = object;
         this.pattern = pattern;
-        
+
         setName("StreamProcessor["+pattern+"]");
+        logger.debug(getName());
     }
-    
+
     public void run() {
         status = Status.ACTIVE;
         matched = false;
-        
-        BufferedReader reader = 
+
+        BufferedReader reader =
             new BufferedReader(new InputStreamReader(istream));
-        PrintStream writer = 
+        PrintStream writer =
             new PrintStream(new BufferedOutputStream(ostream));
-        
+
         String line = null;
         try {
             while ((line = reader.readLine()) != null) {
                 if (line.contains(pattern)) {
                     writer.flush();
                     matched = true;
-                    synchronized(object) { 
+                    synchronized(object) {
                         object.notify();
+                        break;
                     }
                 }
-                else 
+                else
                     writer.println(line);
             }
             writer.flush();
@@ -62,7 +68,7 @@ public class StreamProcessor extends Streamer {
         }
         status = Status.COMPLETED;
     }
-    
+
     public boolean matched() {
         return matched;
     }
