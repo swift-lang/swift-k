429e86cc9ef3933fcf161f81f5cce10188445bc7
Sarah Kenny <skenny@uchicago.edu>
2011-01-25 06:54:23 +0000

    setting cog and swift versions and site env variables
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.92@4034 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/run-nightly.sh
+++ tests/run-nightly.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-# Sketch of script that is called on remote test site
+# example file for setting up environment and running nightly.sh
+# given a groupslistfile, will run nightly.sh on that site
 
-GROUPLISTFILE=$1 # E.g., groups/group-all-local.sh
+GROUPSLISTFILE=$1 # E.g., groups/group-all-local.sh
 
-# Enter test directory
-cd $( dirname $0 ) || exit 1
+# user-specific variables
 
-svn up || exit 1
+export WORK=/ci/projects/cnari/swift_work/skenny
+export QUEUE=short
+export PROJECT=CI-IBN000039
 
-# Work within, e.g., /home/wozniak/nightly/topdir
-rm -rf topdir
-./nightly.sh -g -o topdir $GROUPLISTFILE
-[ $? != 0 ] && exit 1
+# branch testing
+
+export COG_VERSION=branches/4.1.8
+export SWIFT_VERSION=branches/release-0.92
+
+/home/skenny/swift_runs/tests/nightly.sh -g $GROUPSLISTFILE
 
 exit 0
