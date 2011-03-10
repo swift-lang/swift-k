bf091ec627eb73f2253948de7e0d88b4fa14085c
Mihael Hategan <hategan@mcs.anl.gov>
2011-03-10 18:33:49 +0000

    added dynamic buffer size management
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current@3057 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/LocalTCPService.java
+++ src/cog/modules/provider-coaster/src/org/globus/cog/abstraction/coaster/service/LocalTCPService.java
@@ -27,6 +27,7 @@ public class LocalTCPService extends GSSService implements Registering {
     public static final int TCP_BUFSZ = 32768;
     
     private RegistrationManager registrationManager;
+    private TCPBufferManager buffMan;
     
     // private int idseq;
     
@@ -35,11 +36,13 @@ public class LocalTCPService extends GSSService implements Registering {
     public LocalTCPService(RequestManager rm) throws IOException {
         super(false, 0);
         setRequestManager(rm);
+        buffMan = new TCPBufferManager();
     }
 
     public LocalTCPService(RequestManager rm, int port) throws IOException {
         super(false, port);
         setRequestManager(rm);
+        buffMan = new TCPBufferManager();
     }
 
     public String registrationReceived(String blockid, String url, KarajanChannel channel) throws ChannelException {
@@ -69,13 +72,12 @@ public class LocalTCPService extends GSSService implements Registering {
 
     protected void handleConnection(Socket socket) {
         try {
-            socket.setReceiveBufferSize(TCP_BUFSZ);
-            socket.setSendBufferSize(TCP_BUFSZ);
+            buffMan.addSocket(socket);
             socket.setTcpNoDelay(true);
         }
         catch (SocketException e) {
             e.printStackTrace();
         }
-        super.handleConnection(socket);
+        super.handleConnection(buffMan.wrap(socket));
     }
 }
