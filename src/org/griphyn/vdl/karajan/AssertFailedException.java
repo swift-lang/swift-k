2b8d6fa46538b3fc9c28bbd1cf7702012748676d
Justin Wozniak <wozniak@mcs.anl.gov>
2011-04-04 15:37:22 +0000

    Possibly better class definition
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4256 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/AssertFailedException.java
+++ src/org/griphyn/vdl/karajan/AssertFailedException.java
@@ -1,13 +1,15 @@
 
 package org.griphyn.vdl.karajan;
 
+import org.globus.cog.karajan.workflow.ExecutionException;
+
 /** 
  * Generated only by SwiftScript @assert(). 
  * 
  * Created on September 27, 2010
  * @author wozniak
  */
-public class AssertFailedException extends RuntimeException {
+public class AssertFailedException extends ExecutionException {
 
     private static final long serialVersionUID = 1L;
    
