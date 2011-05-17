f7b79122cd0e9361864214727174196585d0dcd9
David Kelly <davidkelly@uchicago.edu>
2011-05-17 15:30:41 +0000

    Updates for doc build scripts and guides
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4483 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+# crash: Report a problem and exit
+crash()
+{
+    MSG=$1
+    echo ${MSG}  >&2
+    exit 1
+}
+
+# Verify correct arguments
 if [ -n "$1" ]; then
    INSTALLATION_DIRECTORY=$1
 else
-   echo Error: Must specify a directory for installation
-   exit 1
+   crash "Error: Must specify a directory for installation"
 fi
 
+# Create installation directory if needed
 if [ ! -d "$INSTALLATION_DIRECTORY" ]; then
-   mkdir $INSTALLATION_DIRECTORY || exit 1
+   mkdir $INSTALLATION_DIRECTORY || crash "Unable to create directory $INSTALLATION_DIRECTORY"
 fi
 
+# Gather version information
+pushd .. > /dev/null 2>&1
+VERSION=`svn info |grep URL|awk -F / '{print $NF}'`
+popd > /dev/null 2>&1
 echo Installing docs into $INSTALLATION_DIRECTORY
 
-cd docs || exit 1
+# Convert files
 DIRECTORIES=*/
 for directory in $DIRECTORIES
 do
-   cd $directory || exit 1
+   pushd $directory > /dev/null 2>&1
    FILES=*.txt
    for file in $FILES
    do
@@ -26,8 +39,15 @@ do
       echo Converting $directory"$file" to PDF
       a2x --format=pdf --no-xmllint $file 
    done
-   cp *.html $INSTALLATION_DIRECTORY || exit 1
-   cp *.pdf $INSTALLATION_DIRECTORY || exit 1
-   cd .. || exit 1
+
+   if [ ! -d "$INSTALLATION_DIRECTORY/$VERSION" ]; then
+      mkdir $INSTALLATION_DIRECTORY/$VERSION || crash "Unable to create directory $INSTALLATION_DIRECTORY/$VERSION"
+   fi
+
+   if [ ! -d "$INSTALLATION_DIRECTORY/$VERSION/$directory" ]; then
+      mkdir $INSTALLATION_DIRECTORY/$VERSION/$directory || crash "Unable to create directory $INSTALLATION_DIRECTORY/$VERSION/$directory"
+   fi
+   cp *.html $INSTALLATION_DIRECTORY/$VERSION/$directory || crash "Unable to copy html files to $INSTALLATION_DIRECTORY/$VERSION/$directory"
+   cp *.pdf $INSTALLATION_DIRECTORY/$VERSION/$directory || crash "Unable to copy pdf files to $INSTALLATION_DIRECTORY/$VERSION/$directory"
+   popd > /dev/null 2>&1
 done
-cd ..
