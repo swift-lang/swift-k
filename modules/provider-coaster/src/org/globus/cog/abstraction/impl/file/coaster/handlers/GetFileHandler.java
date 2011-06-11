99284a18646ec6f2ef9cbdd1a1ae285515cd97ed
Mihael Hategan <hategan@mcs.anl.gov>
2011-06-11 01:41:03 +0000

    updated coaster file resource
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src/cog@3147 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- modules/provider-coaster/src/org/globus/cog/abstraction/impl/file/coaster/handlers/GetFileHandler.java
+++ modules/provider-coaster/src/org/globus/cog/abstraction/impl/file/coaster/handlers/GetFileHandler.java
@@ -34,7 +34,6 @@ public class GetFileHandler extends CoasterFileRequestHandler implements SendCal
     public void requestComplete() throws ProtocolException {
         String src = getInDataAsString(0);
         try {
-            logger.debug("for: " + src);
             provider = IOProviderFactory.getDefault().instance(getProtocol(src));
             sendReply();
         }
