c53d6c52afaabc3f5dbf1e0be4bed38f4bd99216
Mihael Hategan <hategan@mcs.anl.gov>
2011-06-14 02:17:30 +0000

    fixed arguments when wrapper arguments are passed through files
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4612 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/UnwrapClosedList.java
+++ src/org/griphyn/vdl/karajan/lib/UnwrapClosedList.java
@@ -3,17 +3,14 @@
  */
 package org.griphyn.vdl.karajan.lib;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.globus.cog.karajan.arguments.Arg;
 import org.globus.cog.karajan.stack.VariableStack;
 import org.globus.cog.karajan.workflow.ExecutionException;
-import org.globus.cog.karajan.workflow.futures.FutureNotYetAvailable;
 import org.griphyn.vdl.mapping.DSHandle;
-import org.griphyn.vdl.mapping.HandleOpenException;
-import org.griphyn.vdl.mapping.InvalidPathException;
-import org.griphyn.vdl.mapping.Path;
 
 public class UnwrapClosedList extends VDLFunction {
 	public static final Logger logger = Logger.getLogger(UnwrapClosedList.class);
@@ -24,18 +21,14 @@ public class UnwrapClosedList extends VDLFunction {
 		setArguments(UnwrapClosedList.class, new Arg[] { PA_LIST });
 	}
 
-	/**
-	 * Takes a supplied variable and path, and returns the unique value at that
-	 * path. Path can contain wildcards, in which case an array is returned.
-	 */
 	public Object function(VariableStack stack) throws ExecutionException {
 		@SuppressWarnings("unchecked")
         List<DSHandle> l = (List<DSHandle>) PA_LIST.getValue(stack);
 		
-		Object[] r = new Object[l.size()];
+		List<Object> r = new ArrayList<Object>(l.size());
 		
-		for (int i = 0; i < r.length; i++) {
-		    r[i] = l.get(i).getValue();
+		for (DSHandle h : l) {
+		    r.add(h.getValue());
 		}
 		
 		return r;
