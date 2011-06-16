319b6106093f0d71ad685b816a8144666869dbd0
Alberto Chavez <alberto_chavez@live.com>
2011-06-16 21:30:08 +0000

    Added the newest groups to the script
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4625 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/groups/group-all-local.sh
+++ tests/groups/group-all-local.sh
@@ -2,13 +2,23 @@
 # GROUPLIST definition to run all local tests
 
 GROUPLIST=( $TESTDIR/language-behaviour \
-            $TESTDIR/language/working \
-            $TESTDIR/local \
-            $TESTDIR/language/should-not-work \
-            $TESTDIR/cdm \
-            $TESTDIR/cdm/ps \
-            $TESTDIR/cdm/star
-            $TESTDIR/cdm/ps/pinned
+            # $TESTDIR/language/working \
+            # $TESTDIR/local \
+            # $TESTDIR/language/should-not-work \
+            # $TESTDIR/cdm \
+            # $TESTDIR/cdm/ps \
+            # $TESTDIR/cdm/star
+            $TESTDIR/language-behaviour/arrays \
+	    $TESTDIR/language-behaviour/mappers \
+	    $TESTDIR/language-behaviour/IO \
+	    $TESTDIR/language-behaviour/iterators \
+	    $TESTDIR/language-behaviour/logic \
+	    $TESTDIR/language-behaviour/control_structures \
+	    $TESTDIR/language-behaviour/procedures \
+	    $TESTDIR/language-behaviour/arithmetic \
+            $TESTDIR/language-behaviour/strings \
+	    $TESTDIR/documentation/ \
+	    # $TESTDIR/cdm/ps/pinned
 	    # $TESTDIR/site/intrepid
           )
 
