6212ee41e6197f16d7c182945264cabd4e267e0f
Mihael Hategan <hategan@mcs.anl.gov>
2011-07-07 23:27:17 +0000

    s/OUTPUT/TEST_LOG/
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4800 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/language-behaviour/mappers/07511-fixed-array-mapper-input.check.sh
+++ tests/language-behaviour/mappers/07511-fixed-array-mapper-input.check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-cat $OUTPUT | grep "file: 07511-fixed-array-mapper-input.first.in" || exit 1
-cat $OUTPUT | grep "file: 07511-fixed-array-mapper-input.second.in" || exit 1
-cat $OUTPUT | grep "file: 07511-fixed-array-mapper-input.third.in" || exit 1
+cat $TEST_LOG | grep "file: 07511-fixed-array-mapper-input.first.in" || exit 1
+cat $TEST_LOG | grep "file: 07511-fixed-array-mapper-input.second.in" || exit 1
+cat $TEST_LOG | grep "file: 07511-fixed-array-mapper-input.third.in" || exit 1
 exit 0
