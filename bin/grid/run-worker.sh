dbf46e02ebf5a1e00fb55a19ac792eefabf1b81e
Michael Wilde <wilde@mcs.anl.gov>
2011-08-09 21:53:47 +0000

    Snapshop of bin/grid - tested to 2M+ jobs with catsn. Adds run-worker wrapper and swiftDemand file, and interim siteguide text.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@4970 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ bin/grid/run-worker.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+contact=$1
+workername=$2
+origlogdir=$3
+echo OSG_WN_TMP=$OSG_WN_TMP
+if [ _$OSG_WN_TMP = _ ]; then
+  OSG_WN_TMP=/tmp
+fi
+mkdir -p $OSG_WN_TMP
+logdir=$(mktemp -d $OSG_WN_TMP/${workername}.workerdir.XXXXXX)
+nlines=1000
+
+echo "=== contact: $contact"
+echo "=== name:    $workername Running in dir $(pwd)"
+echo "=== cwd:     $(pwd)"
+echo "=== logdir:  $logdir"
+echo "==============================================="
+
+cat >worker.pl
+chmod +x worker.pl
+
+./worker.pl $contact $workername $logdir
+
+exitcode=$?
+
+echo "=== exit: worker.pl exited with code=$exitcode"
+
+echo "=== worker log - last $nlines lines:"
+
+echo
+
+tail -v -n $nlines $logdir/*
