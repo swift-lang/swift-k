ed48e7c206d46abd05e3e5a8b0381110d4a93d5b
Justin Wozniak <wozniak@mcs.anl.gov>
2011-08-30 18:28:40 +0000

    Copy Beagle test for Fusion
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5029 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/mpi/fusion/long/100-mci.setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+which mpicc || exit 1
+
+# mpicc -std=gnu99 $GROUP/100-mpi-cp.c -o $GROUP/mpi-cp || exit 1
+mpicc -std=gnu99 $GROUP/mpi-sleep.c -o $GROUP/mpi-sleep || exit 1
+
+cp -v $GROUP/100-input.txt . || exit 1
+
+exit 0
