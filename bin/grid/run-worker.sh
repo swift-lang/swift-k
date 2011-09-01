35070d03357b8aedc856db3b276c0c6b93c6d81e
Michael Wilde <wilde@mcs.anl.gov>
2011-09-01 21:09:25 +0000

    fixed problem in foreachsite; edits to run-worker.
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5054 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- bin/grid/run-worker.sh
+++ bin/grid/run-worker.sh
@@ -12,6 +12,8 @@ mkdir -p $OSG_WN_TMP
 logdir=$(mktemp -d $OSG_WN_TMP/${workername}.workerdir.XXXXXX)
 nlines=1000
 
+PATH=$OSG_APP/extenci/aashish/terfix/bin:$PATH
+
 echo "=== contact: $contact"
 echo "=== name:    $workername Running in dir $(pwd)"
 echo "=== cwd:     $(pwd)"
