2fc6588be32bb4471ea7931e1268cd2b5b9cd559
Justin Wozniak <wozniak@mcs.anl.gov>
2011-11-15 00:07:09 +0000

    Correction to test case SwiftScript
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5298 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/cdm/211-direct-output-abs.swift
+++ tests/cdm/211-direct-output-abs.swift
@@ -9,6 +9,6 @@ app (file o) copy (file i)
 file f1<"211-input.txt">;
 
 // Will be transformed by CDM to /tmp/211-output.txt:
-file f2<"211-output.txt">;
+file f2<"/tmp/211-output.txt">;
 
 f2 = copy(f1);
