10dd8691935329db175cfc447ab8539444880754
David Kelly <davidkelly@uchicago.edu>
2011-12-02 17:40:32 +0000

    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.93@5333 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- bin/vm/ec2/lib/python2.7/site-packages/globus_provision-0.3.2-py2.7.egg/globus/provision/chef-files/roles/domain-myproxy.rb
+++
@@ -1,4 +0,0 @@
-name "domain-myproxy"
-description "A domain's MyProxy server"
-run_list "role[globus]", "recipe[provision::simpleca]", "recipe[provision::ca]", "recipe[globus::myproxy]"
-
--- bin/vm/ec2/lib/python2.7/site-packages/globus_provision-0.3.2-py2.7.egg/globus/provision/chef-files/roles/domain-nfsnis-client.rb
+++
@@ -1,4 +0,0 @@
-name "domain-nfsnis-client"
-description "A domain's client machine (any machine that is not the NFS/NIS server)"
-run_list "recipe[provision::gp_node]", "recipe[provision::nis_client]", "recipe[provision::nfs_client]"
-
