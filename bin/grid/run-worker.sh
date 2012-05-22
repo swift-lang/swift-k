3a281207113bc0c3eeb1751624b15c6365bbfcc2
David Kelly <davidkelly@uchicago.edu>
2012-05-22 15:25:28 +0000

    A few updates for better logging
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5791 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- bin/grid/run-worker.sh
+++ bin/grid/run-worker.sh
@@ -4,14 +4,13 @@ contact=$1
 workername=$2
 origlogdir=$3
 echo OSG_WN_TMP=$OSG_WN_TMP
-if [ _$OSG_WN_TMP = _ ]; then
-  OSG_WN_TMP=/tmp
-fi
 
+#if [ _$OSG_WN_TMP = _ ]; then
+#  OSG_WN_TMP=/tmp
+#fi
 mkdir -p $OSG_WN_TMP
-logdir=$(mktemp -d $OSG_WN_TMP/${workername}.workerdir.XXXXXX)
-nlines=1000
 
+logdir=$(mktemp -d $OSG_WN_TMP/${workername}.workerdir.XXXXXX)
 export PATH=${OSG_APP}/engage/scec:${OSG_APP}/engage/scec/JBSim3d/bin:${OSG_APP}/engage/scec/SpectralAcceleration/p2utils:${OSG_APP}/extenci/swift/DSSAT/bin:${OSG_APP}/extenci/aashish/terfix/bin:$PATH
 #export PATH=${OSG_PATH}:$PATH
 
@@ -19,6 +18,8 @@ echo "=== contact: $contact"
 echo "=== name:    $workername Running in dir $(pwd)"
 echo "=== cwd:     $(pwd)"
 echo "=== logdir:  $logdir"
+echo "=== disk space: "
+df -h
 echo "=== path:    $PATH"
 echo "==============================================="
 
@@ -31,9 +32,14 @@ exitcode=$?
 
 echo "=== exit: worker.pl exited with code=$exitcode"
 
-echo "=== worker log - last $nlines lines:"
+echo "=== worker logs:"
 
 echo
 
-tail -v -n $nlines $logdir/*
+for file in $logdir/*
+do
+   echo $file
+   echo -------
+   cat $file
+done
 
