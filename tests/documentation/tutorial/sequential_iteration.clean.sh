e8c1f71a91240ab54a4e82d8a1d3677d95009e9f
David Kelly <davidkelly@uchicago.edu>
2012-06-08 22:00:59 +0000

    use the updated sequential iteration script from the tutorial
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5808 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/documentation/tutorial/sequential_iteration.clean.sh
+++ tests/documentation/tutorial/sequential_iteration.clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-rm -rf sequential_iteration-* sequential_iteration.kml sequential_iteration.xml foldout* || exit 1
+rm -rf sequential_iteration-* sequential_iteration.kml sequential_iteration.xml || exit 1
 exit 0
