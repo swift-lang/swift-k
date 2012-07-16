5aa2340f450219deed0f50ba77e94059811e67f2
Mihael Hategan <hategan@mcs.anl.gov>
2012-07-16 23:19:44 +0000

    better trace name
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5838 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/InfiniteCountingWhile.java
+++ src/org/griphyn/vdl/karajan/lib/InfiniteCountingWhile.java
@@ -97,4 +97,11 @@ public class InfiniteCountingWhile extends Sequential {
             return;
         }
     }
+
+    @Override
+    public String getTextualName() {
+        return "iterate";
+    }
+    
+    
 }
