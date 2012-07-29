5ffed0ec289cfb5d82289c9f765ab8b36e072d7d
David Kelly <davidkelly@uchicago.edu>
2012-07-29 22:28:10 +0000

    Initial commit of slurm scheduler
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk/current/src@3432 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

---
+++ cog/modules/provider-localscheduler/src/org/globus/cog/abstraction/impl/scheduler/slurm/execution/TaskHandlerImpl.java
@@ -0,0 +1,21 @@
+package org.globus.cog.abstraction.impl.scheduler.slurm.execution;
+
+import org.globus.cog.abstraction.interfaces.DelegatedTaskHandler;
+
+/**
+ *Provides a local PBS <code>TaskHandler</code>
+ *for job submission to the local resource without
+ *any security context.
+ *
+ */
+public class TaskHandlerImpl extends
+		org.globus.cog.abstraction.impl.common.execution.TaskHandlerImpl {
+
+	protected DelegatedTaskHandler newDelegatedTaskHandler() {
+		return new JobSubmissionTaskHandler();
+	}
+
+	protected String getName() {
+		return "Slurm";
+	}
+}
