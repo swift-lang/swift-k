923355955f25542910e3719751f1187987bb4d92
Mihael Hategan <hategan@mcs.anl.gov>
2012-09-14 20:33:20 +0000

    max stats period got changed by accident to 3s. revert back to 30s
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@5921 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/RuntimeStats.java
+++ src/org/griphyn/vdl/karajan/lib/RuntimeStats.java
@@ -44,7 +44,7 @@ public class RuntimeStats extends FunctionsCollection {
 	public static SimpleDateFormat formatter = 
 		new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss Z");
 	public static final int MIN_PERIOD_MS=1000;
-	public static final int MAX_PERIOD_MS=3000;
+	public static final int MAX_PERIOD_MS=30000;
 
 	public static final String[] preferredOutputOrder = {
 		"uninitialized",
