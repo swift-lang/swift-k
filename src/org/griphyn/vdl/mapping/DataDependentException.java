36da1281ddd16883f3cb222c90b4188626a68363
Mihael Hategan <hategan@mcs.anl.gov>
2012-11-10 05:34:58 +0000

    better error message for dependent exceptions
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@6021 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/mapping/DataDependentException.java
+++ src/org/griphyn/vdl/mapping/DataDependentException.java
@@ -35,7 +35,7 @@ public class DataDependentException extends DependentException {
 	}
 
 	public String getMessage() {
-		return getHandle() + " not derived due to errors in data dependencies";
+		return getVariableName() + " not derived due to errors in data dependencies";
 	}
 
 }
