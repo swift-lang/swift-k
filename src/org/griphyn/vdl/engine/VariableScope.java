2fcb3d8f0c56da9ac073926a44d3dba18ee61341
Mihael Hategan <hategan@mcs.anl.gov>
2013-03-08 18:48:02 +0000

    properly handle inhibitClosing in else branch if a close was added to match what is in the then branch
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.94@6349 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/engine/VariableScope.java
+++ src/org/griphyn/vdl/engine/VariableScope.java
@@ -308,6 +308,9 @@ public class VariableScope {
 	        inhibitClosing = new HashSet<String>();
 	    }
 		inhibitClosing.add(name);
+		if (thenScope != null) {
+		    setPreClose(name, 0);
+		}
 	}
 
 	public void addVariable(String name, String type, String context, boolean global, XmlObject src) throws CompilationException {
@@ -650,7 +653,7 @@ public class VariableScope {
 	}
 			
 	private void setPreClose(String name, int count) {
-	    if (inhibitClosing != null && inhibitClosing.contains(name)) {
+	    if (inhibitClosing != null && inhibitClosing.contains(name) && count != 0) {
 	        return;
 	    }
 	    setCount("preClose", name, count, 
