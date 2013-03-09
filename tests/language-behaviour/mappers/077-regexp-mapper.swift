602b4d5c7d501d0f67a25c9b6013942a21d8d6cc
Ketan Maheshwari <ketan@mcs.anl.gov>
2013-03-09 05:46:40 +0000

    improve regexp mapper test
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.93@6357 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/language-behaviour/mappers/077-regexp-mapper.swift
+++ tests/language-behaviour/mappers/077-regexp-mapper.swift
@@ -6,10 +6,10 @@ type messagefile;
     }
 }
 
-messagefile outfile <regexp_mapper;
-       source="outfoo",
-       match="(...)(.*)",
-       transform="077-regexp-mapper.\\2.\\1">;
+messagefile infile <"077-regexpmapper-input.in">;
+
+messagefile outfile <regexp_mapper; source="infile",
+       match="(.*)in", transform=@strcat("077-regexp-mapper.foo.","\\1out")>;
 
 outfile = greeting();
 
