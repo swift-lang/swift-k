3a5d4c586ec4b8c1925a1533472facf22d332b91
David Kelly <davidkelly@uchicago.edu>
2013-07-10 18:54:18 +0000

    Old groups
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.94@6631 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/groups/group-pads.sh
+++
@@ -1,8 +0,0 @@
-
-# GROUPLIST definition to run pbs tests
-
-GROUPLIST=( 	    
-	    $TESTDIR/providers/local \
-            $TESTDIR/providers/pads/coasters \
-	    $TESTDIR/providers/pads/pbs \
-)
--- tests/groups/group-queenbee.sh
+++
@@ -1,6 +0,0 @@
-# GROUPLIST definition to run queenbee tests
-
-GROUPLIST=( 	    
-	    $TESTDIR/providers/local \
-	    $TESTDIR/providers/queenbee/pbs \
-)
