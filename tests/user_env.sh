cb48fbbba9bc82836285a49726bf0dd19826a875
Yadu Nand B <yadudoc1729@gmail.com>
2013-07-10 20:13:28 +0000

    Committing minor changes
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.94@6635 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

---
+++ tests/user_env.sh
@@ -0,0 +1,10 @@
+export BEAGLE_USERNAME=""
+export MIDWAY_USERNAME=""
+export MCS_USERNAME=""
+export UC3_USERNAME=""
+
+if [ "midway001" == "midway001" ]
+then
+   export GLOBUS_HOSTNAME=swift.rcc.uchicago.edu
+   export GLOBUS_TCP_PORT_RANGE=50000,51000
+fi;
