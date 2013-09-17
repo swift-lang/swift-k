0475bae81dd2a52a0d484b262af203cd76e8e7a3
Mihael Hategan <hategan@mcs.anl.gov>
2013-09-17 03:04:38 +0000

    also dump waiting/sleeping/blocked JVM threads if everything else fails
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@7077 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/HangChecker.java
+++ src/org/griphyn/vdl/karajan/HangChecker.java
@@ -108,6 +108,9 @@ public class HangChecker extends TimerTask {
                     if (!found) {
                         found = findJVMDeadlocks(ps);
                     }
+                    if (!found) {
+                        dumpJVMThreads(ps);
+                    }
                     logger.warn(os.toString());
                     ps.close();
                     if (found) {
@@ -122,9 +125,26 @@ public class HangChecker extends TimerTask {
         }
     }
     
+    private void dumpJVMThreads(PrintStream pw) {
+        ThreadMXBean b = ManagementFactory.getThreadMXBean();
+        if (b != null) {
+            long[] ids = b.getAllThreadIds();
+            if (ids != null && ids.length != 0) {
+                ThreadInfo[] tis = b.getThreadInfo(ids, true, true); 
+                pw.println("\nWaiting JVM threads:");
+                for (ThreadInfo ti : tis) {
+                    Thread.State state = ti.getThreadState();
+                    if (state != Thread.State.RUNNABLE && state != Thread.State.TERMINATED) {
+                        printThreadInfo(pw, ti);
+                    }
+                }
+            }
+        }
+    }
+
     private boolean findJVMDeadlocks(PrintStream pw) {;
-        try {
-            ThreadMXBean b = ManagementFactory.getThreadMXBean();
+        ThreadMXBean b = ManagementFactory.getThreadMXBean();
+        if (b != null) {
             long[] ids = b.findDeadlockedThreads();
             if (ids != null && ids.length != 0) {
                 ThreadInfo[] tis = b.getThreadInfo(ids, true, true); 
@@ -134,9 +154,6 @@ public class HangChecker extends TimerTask {
                 }
                 return true;
             }
-        }
-        catch (Exception e) {
-            logger.warn("Exception caught trying to find JVM deadlocks", e);
         }
         return false;
     }
