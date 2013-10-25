ff3c41b35b2b644c50cc846833531b4b6abbc391
Yadu Nand B <yadudoc1729@gmail.com>
2013-10-25 21:25:05 +0000

    Issue with destructor invoked at end of block fixed. CoasterSWIGClientSettings, works with no crashes.
    Added :
    CoasterLoop* CoasterSWIGLoopCreate(void);
    int CoasterSWIGLoopDestroy(CoasterLoop* loop);
    int CoasterSWIGClientDestroy(CoasterClient *client);
    Tests for above changes.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/cogkit/svn/trunk@3819 5b74d2a0-fa0e-0410-85ed-ffba77ec0bde

--- src/cog/modules/provider-coaster-c-client/src/CoasterSWIG.h
+++ src/cog/modules/provider-coaster-c-client/src/CoasterSWIG.h
@@ -1,14 +1,24 @@
 
 #include "CoasterClient.h"
+//#include "CoasterLoop.h"
 
 #ifdef SWIG
 %module coaster
 %{
   #include "CoasterClient.h"
   #include "CoasterSWIG.h"
+  #include "CoasterLoop.h"
 %}
 #endif
 
-CoasterClient* CoasterSWIGClientCreate(char* serviceURL);
+CoasterLoop* CoasterSWIGLoopCreate(void);
+int CoasterSWIGLoopDestroy(CoasterLoop* loop);
+
+CoasterClient* CoasterSWIGClientCreate(CoasterLoop &loop, char* serviceURL);
+int CoasterSWIGClientDestroy(CoasterClient *client);
 
 int CoasterSWIGClientSettings(CoasterClient *client, char *settings);
+
+
+
+
---
+++ src/cog/modules/provider-coaster-c-client/tests/tcl/coaster-loop-start-stop.tcl
@@ -0,0 +1,11 @@
+load ../../src/.libs/libcoasterclient.so
+
+set loop_ptr [CoasterSWIGLoopCreate]
+set client_ptr [CoasterSWIGClientCreate $loop_ptr 140.221.8.81:40568]
+
+set rcode [CoasterSWIGClientDestroy $client_ptr]
+set rcode [CoasterSWIGLoopDestroy $loop_ptr]
+
+
+
+
