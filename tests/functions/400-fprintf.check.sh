9df4cf358b7aee9a3dadbf7bc0c4dd0b4bbb4705
David Kelly <davidkelly@uchicago.edu>
2013-11-25 16:33:20 +0000

    Fixes for leading whitespace causing failures
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@7330 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/functions/400-fprintf.check.sh
+++ tests/functions/400-fprintf.check.sh
@@ -7,7 +7,7 @@ FILES=( $( ls 400-fprintf-*.out ) )
 
 grep hello 400-fprintf-1.out || exit 1
 
-LINES=$( wc -l 400-fprintf-2.out | cut -d ' ' -f 1 )
+LINES=$( wc -l 400-fprintf-2.out | sed 's/^[ \t]*//' | cut -d ' ' -f 1 )
 (( ${LINES} == 3 )) || exit 1
 
 grep "hello: 32" 400-fprintf-3.out || exit 1
