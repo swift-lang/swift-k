bf36c330a7467c17b8222fbf7536e4b152c71426
David Kelly <davidkelly@uchicago.edu>
2014-03-11 19:22:25 +0000

    uc3-sub is being taken down and no longer functioning, use osgconnect instead
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/branches/release-0.94@7693 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- tests/groups/group-apps.sh
+++ tests/groups/group-apps.sh
@@ -5,8 +5,8 @@ GROUPLIST=(
             $TESTDIR/stress/apps/modis_beagle  \
 	    $TESTDIR/stress/apps/modis_local   \
 	    $TESTDIR/stress/apps/modis_midway  \
-	    $TESTDIR/stress/apps/modis_uc3     \
-	    $TESTDIR/stress/apps/modis_multiple\
+            $TESTDIR/stress/apps/modis_osgc \
+	    $TESTDIR/stress/apps/modis_multiple \
           )
 
 checkvars WORK
