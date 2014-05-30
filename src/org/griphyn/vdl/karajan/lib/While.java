58c4823ea6e3712259de40325acc5c6723cf0c80
Mihael Hategan <hategan@mcs.anl.gov>
2014-05-30 23:25:27 +0000

    fixed thread id issue in iterate() which can cause duplicate mapping errors
    git-svn-id: https://svn.ci.uchicago.edu/svn/vdl2/trunk@7890 e2bb083e-7f23-0410-b3a8-8253ac9ef6d8

--- src/org/griphyn/vdl/karajan/lib/While.java
+++ src/org/griphyn/vdl/karajan/lib/While.java
@@ -12,6 +12,7 @@ package org.griphyn.vdl.karajan.lib;
 import java.util.LinkedList;
 import java.util.List;
 
+import k.rt.KRunnable;
 import k.rt.SingleValueChannel;
 import k.rt.Stack;
 import k.thr.LWThread;
@@ -80,42 +81,55 @@ public class While extends InternalFunction {
         int i = thr.checkSliceAndPopState();
         SingleValueChannel<Object> next = (SingleValueChannel<Object>) thr.popState();
         List<RefCount> drefs = (List<RefCount>) thr.popState();
+        LWThread thr2 = (LWThread) thr.popState();
         Stack stack = thr.getStack();
         try {
-            switch(i) {
-                case 0:
-                    drefs = RefCount.build(stack, srefs);
-                    var.setValue(stack, NodeFactory.newRoot(Field.GENERIC_INT, 0));
-                    c_next.create(stack);
-                    RefCount.incRefs(drefs);
-                    next = (SingleValueChannel<Object>) c_next.get(stack);
-                    if (tracer.isEnabled()) {
-                        tracer.trace(thr, unwrap(next));
-                    }
-                    i++;
-                case 1:
-                    while (true) {
-                        body.run(thr);
-                        if (next.isEmpty()) {
-                            // must do this twice since the closeDataSet calls
-                            // inside the iterate won't be called if the iterate 
-                            // condition is true
-                            RefCount.decRefs(drefs);
-                            RefCount.decRefs(drefs);
-                            break;
-                        }
-                        else {
+            out:
+                while(true) {
+                    switch(i) {
+                        case 0:
+                            drefs = RefCount.build(stack, srefs);
+                            var.setValue(stack, NodeFactory.newRoot(Field.GENERIC_INT, 0));
+                            c_next.create(stack);
                             RefCount.incRefs(drefs);
-                        }
-                        Object val = next.removeFirst();
-                        if (tracer.isEnabled()) {
-                            tracer.trace(thr, unwrap(next));
-                        }
-                        var.setValue(stack, val);
+                            next = (SingleValueChannel<Object>) c_next.get(stack);
+                            if (tracer.isEnabled()) {
+                                tracer.trace(thr, unwrap(next));
+                            }
+                            i++;
+                        case 1:
+                            thr2 = thr.fork(new KRunnable() {
+                                @Override
+                                public void run(LWThread thr) {
+                                    body.run(thr);
+                                }
+                            });
+                            i++;
+                            thr2.start();
+                        case 2:
+                            thr2.waitFor();
+                            if (next.isEmpty()) {
+                                // must do this twice since the closeDataSet calls
+                                // inside the iterate won't be called if the iterate 
+                                // condition is true
+                                RefCount.decRefs(drefs);
+                                RefCount.decRefs(drefs);
+                                break out;
+                            }
+                            else {
+                                RefCount.incRefs(drefs);
+                            }
+                            Object val = next.removeFirst();
+                            if (tracer.isEnabled()) {
+                                tracer.trace(thr, unwrap(next));
+                            }
+                            var.setValue(stack, val);
+                            i = 1;
                     }
-            }
+                }
         }
         catch (Yield y) {
+            y.getState().push(thr2);
             y.getState().push(drefs);
             y.getState().push(next);
             y.getState().push(i);
