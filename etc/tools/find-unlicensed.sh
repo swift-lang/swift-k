59bb7bd90673445e1d15a91346a2d898e729bca5
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-30 16:38:40 -0500

    Add headers to swift java files missing them

---
+++ etc/tools/find-unlicensed.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+if [ $# > 0 ]
+then
+  dir=$1
+else
+  dir=modules
+fi
+
+find $dir -type f | (
+  set -e
+  while read file
+  do
+    if [[ $file =~ .*\.class ]] || \
+       [[ $file =~ .*\.jar ]] ||   \
+       [[ $file =~ .*\.lo ]] ||    \
+       [[ $file =~ .*\.o ]] ||     \
+       [[ $file =~ .*\.Plo ]] ||    \
+       [[ $file =~ .*\.Po ]] ||     \
+       [[ $file =~ .*\.la ]] ||       \
+       [[ $file =~ .*\.jpg ]] ||       \
+       [[ $file =~ .*/\.project ]]
+    then
+      # Compiled files dont need license
+      :
+    elif grep -q "is developed as part of the Java CoG Kit" $file
+    then
+      # It has a license
+      :
+    elif grep -q "Copyright .* University of Chicago" $file
+    then
+      # It has an up-to-date license
+      :
+    else
+      echo $file
+    fi
+  done
+)
--- modules/swift/src/org/griphyn/vdl/karajan/monitor/monitors/swing/SpringUtilities.java
+++ modules/swift/src/org/griphyn/vdl/karajan/monitor/monitors/swing/SpringUtilities.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
  *
