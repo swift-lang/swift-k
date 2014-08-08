7068efa7f97f7900b35ad31bb69164e8ca134f0c
Mike <hategan@mcs.anl.gov>
2014-08-07 23:17:31 -0700

    removed array expansion in ExpandArguments since it is done in unwrapClosedList; also added file name expansion (for when/if it might get enabled)

--- src/org/griphyn/vdl/karajan/lib/ExpandArguments.java
+++ src/org/griphyn/vdl/karajan/lib/ExpandArguments.java
@@ -18,81 +18,45 @@
 package org.griphyn.vdl.karajan.lib;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.Collection;
+import java.util.List;
 
-import k.rt.Channel;
-import k.rt.ExecutionException;
 import k.rt.Stack;
 
 import org.apache.log4j.Logger;
-import org.globus.cog.karajan.analyzer.ChannelRef;
+import org.globus.cog.karajan.analyzer.ArgRef;
 import org.globus.cog.karajan.analyzer.Signature;
-import org.griphyn.vdl.mapping.DSHandle;
-import org.griphyn.vdl.mapping.PathElementComparator;
-import org.griphyn.vdl.mapping.nodes.AbstractDataNode;
-import org.griphyn.vdl.mapping.nodes.ArrayHandle;
+import org.globus.swift.catalog.site.SwiftContact;
+import org.griphyn.vdl.karajan.FileNameExpander;
 
 public class ExpandArguments extends SwiftFunction {
 	public static final Logger logger = Logger.getLogger(ExpandArguments.class);
 	
-	private ChannelRef<Object> c_vargs;
+	private ArgRef<Collection<Object>> args;
+	private ArgRef<SwiftContact> host;
 
 	@Override
     protected Signature getSignature() {
-        return new Signature(params("..."));
+        return new Signature(params("args", "host"));
     }
 
 	public Object function(Stack stack) {
-	    Channel<Object> items = c_vargs.get(stack);
-	    // prefer lower memory usage over speed here
-	    // although the penalty is only there if there are arrays in the arguments
-	    if (hasArrays(items)) {
-	        return expandArrays(items);
+	    Collection<Object> args = this.args.getValue(stack);
+	    SwiftContact host = this.host.getValue(stack);
+	    
+	    String stagingMethod = (String) host.getProperty("stagingMethod");
+	    boolean direct = "direct".equals(stagingMethod);
+	    
+	    List<Object> ret = new ArrayList<Object>();
+	    for (Object a : args) {
+	        if (a instanceof FileNameExpander) {
+	            ((FileNameExpander) a).toString(ret, direct);
+	        }
+	        else {
+	            ret.add(a);
+	        }
 	    }
-	    else {
-	        return items.getAll();
-	    }
+	    return ret;
 	}
-
-	private Object expandArrays(Channel<Object> items) {
-        ArrayList<DSHandle> l = new ArrayList<DSHandle>();
-        for (Object item : items) {
-            AbstractDataNode h = (AbstractDataNode) item;
-            h.waitFor(this);
-            if (item instanceof ArrayHandle) {
-                ArrayHandle array = (ArrayHandle) item;
-                Map<Comparable<?>, DSHandle> m = array.getArrayValue();
-                SortedMap<Comparable<?>, DSHandle> sorted = new TreeMap<Comparable<?>, DSHandle>(new PathElementComparator());
-                sorted.putAll(m);
-                l.addAll(m.values());
-            } 
-            else {
-                l.add(h);
-            }
-            // TODO this does not correctly handle structs or
-            // externals - at the moment, probably neither of
-            // those should be usable as a string. It also
-            // does not handle nested arrays. However, none of
-            // those should get here in normal operation due
-            // to static type-checking
-        }
-        return l;
-    }
-
-    private boolean hasArrays(Channel<Object> items) {
-	    boolean arraySeen = false;
-        for (Object item : items) {
-            if(!(item instanceof DSHandle)) {
-                throw new ExecutionException(this, "Cannot handle argument implemented by " + item.getClass());
-            }
-
-            if (item instanceof ArrayHandle) {
-                arraySeen = true;
-            } 
-        }
-        return arraySeen;
-    }
 }
 
