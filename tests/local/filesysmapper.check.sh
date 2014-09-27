d05d6286af38636bdc12de0a843c9ef949a2770c
Mike <hategan@mcs.anl.gov>
2014-09-27 12:47:17 -0700

    fixed filesys mapper test; the script outputs "./file" and the test expected "file"; allow both

--- tests/local/filesysmapper.check.sh
+++ tests/local/filesysmapper.check.sh
@@ -3,7 +3,7 @@
 set -x
 
 ls filesysmapper.stdout || exit 1
-COUNT=$( grep -c "file: [abc].dat" < filesysmapper.stdout )
+COUNT=$( grep -c "file: \(\./\)\?[abc].dat" < filesysmapper.stdout )
 (( $COUNT == 3 )) || exit 1
 
 exit 0
