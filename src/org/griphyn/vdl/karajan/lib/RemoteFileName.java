e0abaab9c10af296411d6ee465c9fc82a9826639
Mike <hategan@mcs.anl.gov>
2014-09-27 14:10:15 -0700

    fixed error in directory creation with swift staging

---
+++ src/org/griphyn/vdl/karajan/lib/RemoteFileName.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 University of Chicago
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * Created on Jan 5, 2007
+ */
+package org.griphyn.vdl.karajan.lib;
+
+import k.rt.Stack;
+
+import org.globus.cog.karajan.analyzer.ArgRef;
+import org.globus.cog.karajan.analyzer.Signature;
+import org.griphyn.vdl.mapping.AbsFile;
+
+public class RemoteFileName extends SwiftFunction {
+    private ArgRef<AbsFile> file;
+        
+    @Override
+    protected Signature getSignature() {
+        return new Signature(params("file"));
+    }
+
+    @Override
+    public Object function(Stack stack) {
+        AbsFile file = this.file.getValue(stack);
+        return PathUtils.remotePathName(file);
+    }
+}
