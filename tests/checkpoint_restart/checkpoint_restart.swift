61b2f9cd52bbcb7405dcce739ebcbf20150e4eef
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2014-10-13 16:12:05 -0500

    Removed foreach loop to make example simpler

--- tests/checkpoint_restart/checkpoint_restart.swift
+++ tests/checkpoint_restart/checkpoint_restart.swift
@@ -24,24 +24,22 @@ file wrapper <"fibonacci.sh">;
 int  nsim   = toInt(arg("nsim","1"));
 int  nsteps = toInt(arg("nsteps","5"));
 
-file  out[][] <simple_mapper; location="output", prefix="fibonacci", suffix=".out">;
-file  err[][] <simple_mapper; location="output", prefix="fibonacci", suffix=".err">;
-file done[][] <simple_mapper; location="output", prefix="fibonacci", suffix=".done">;
-string isdone[][];
+file  out[] <simple_mapper; location="output", prefix="fibonacci", suffix=".out">;
+file  err[] <simple_mapper; location="output", prefix="fibonacci", suffix=".err">;
+file done[] <simple_mapper; location="output", prefix="fibonacci", suffix=".done">;
+string isdone[];
 
-foreach i in [1:nsim]
-{
-    // Set the initial conditions to enter the iterate loop
-    isdone[i][0] = "notdone";
-    // Set an empty file to be fed to the initial run of app
-    out[i][0] = empty();
+// Set the initial conditions to enter the iterate loop
+isdone[0] = "notdone";
+// Set an empty file to be fed to the initial run of app
+out[0] = empty();
 
-    iterate steps {
-        //tracef("At step : %i\n", steps);
-        (out[i][steps+1], err[i][steps+1], done[i][steps+1]) = fibonacci_randfail (wrapper , 10, 1, out[i][steps]);
-        //(out[i][steps+1], err[i][steps+1], done[i][steps+1]) = fibonacci (wrapper , 10, 1, out[i][steps]);
-        isdone[i][steps+1] = readData( done[i][steps+1] );
-        //tracef("Content of isdone at step %i : %s\n", steps, isdone[i][steps+1]);
-    } until ( isdone[i][steps] == "done" );
-}
+iterate steps {
+
+    (out[steps+1], err[steps+1], done[steps+1]) = fibonacci_randfail (wrapper , nsteps, 1, out[steps]);
+    //(out[steps+1], err[steps+1], done[steps+1]) = fibonacci (wrapper , 10, 1, out[steps]);
+    isdone[steps+1] = readData( done[steps+1] );
+
+} until ( isdone[steps] == "done" );
+
 
