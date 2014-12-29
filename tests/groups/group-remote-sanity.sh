018a1843329dca1bdd301a88b330f6f760626eaf
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2014-12-29 17:15:26 -0600

    Updated remote sanity group with the cloud providers

--- tests/groups/group-remote-sanity.sh
+++ tests/groups/group-remote-sanity.sh
@@ -1,17 +1,29 @@
 # GROUPLIST definition to run all local tests
 
 GROUPLIST=( # Site testing test-group
-            $TESTDIR/sites/beagle \
-            $TESTDIR/sites/mcs    \
-            $TESTDIR/sites/midway \
-            $TESTDIR/sites/osgconnect  \
-            $TESTDIR/sites/mac-frisbee  \
-            $TESTDIR/sites/blues  \
-            $TESTDIR/sites/fusion \
-            # Raven will fail due to firewall restrictions, need to set up a reverse tunnel
-            # $TESTDIR/sites/raven  \
+            $TESTDIR/sites/beagle      \
+            $TESTDIR/sites/blacklight  \
+            $TESTDIR/sites/blues       \
+            $TESTDIR/sites/bridled     \
             $TESTDIR/sites/communicado \
-            $TESTDIR/sites/bridled \
+            $TESTDIR/sites/ec2         \
+            $TESTDIR/sites/fusion      \
+            $TESTDIR/sites/gce         \
+            $TESTDIR/sites/godzilla    \
+            $TESTDIR/sites/local       \
+            $TESTDIR/sites/local-coasters   \
+            $TESTDIR/sites/mac-frisbee \
+            $TESTDIR/sites/mcs         \
+            $TESTDIR/sites/midway      \
+            $TESTDIR/sites/multiple_coaster_pools  \
+            $TESTDIR/sites/osgconnect  \
+            $TESTDIR/sites/raven       \
+            $TESTDIR/sites/ssh-cl-coasters  \
+
+            # Stampede test does not work
+            #$TESTDIR/sites/stampede  \
+            $TESTDIR/sites/swan  \
+
           )
 
 checkvars WORK
