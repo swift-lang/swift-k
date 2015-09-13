baa05b929dfc166f2c71de62c3c0a42de94ab23e
hategan <hategan@mcs.anl.gov>
2015-09-13 13:53:27 -0700

    A less diversified non-buffered once()

--- cogkit/modules/karajan/src/org/globus/cog/karajan/compiled/nodes/Once.java
+++ cogkit/modules/karajan/src/org/globus/cog/karajan/compiled/nodes/Once.java
@@ -28,31 +28,147 @@
  */
 package org.globus.cog.karajan.compiled.nodes;
 
-import k.rt.Context;
-import k.rt.ExecutionException;
-import k.rt.Stack;
+import java.util.LinkedList;
+import java.util.List;
 
+import k.rt.ExecutionException;
+import k.rt.FutureObject;
+import k.rt.Stack;
+import k.thr.LWThread;
+import k.thr.Yield;
+
+import org.globus.cog.karajan.analyzer.ArgRef;
+import org.globus.cog.karajan.analyzer.ChannelRef;
+import org.globus.cog.karajan.analyzer.CompilationException;
+import org.globus.cog.karajan.analyzer.CompilerSettings;
+import org.globus.cog.karajan.analyzer.Scope;
 import org.globus.cog.karajan.analyzer.Signature;
+import org.globus.cog.karajan.analyzer.Var;
+import org.globus.cog.karajan.parser.WrapperNode;
 import org.globus.cog.karajan.util.Cache;
 
-public class Once extends CacheNode {
-	public static final String CACHE = "#once#cache";
-	
+public class Once extends InternalFunction {
+	private ArgRef<Object> on;
+    private Node body;
+    
+    public static final String KEY = "#cachekey";
+
+    private ChannelRef<Object> c_vargs;
+    private ChannelRef<Object> cr_vargs;
+    
+    private Cache cache = new Cache();
+
+    @Override
+    protected Signature getSignature() {
+        return new Signature(params("on", block("body")), returns(channel("...")));
+    }
+    
 	@Override
-	protected Signature getSignature() {
-		return new Signature(params("on", block("body")));
+	protected void compileBlocks(WrapperNode w, Signature sig, LinkedList<WrapperNode> blocks,
+			Scope scope) throws CompilationException {
+	    Var.Channel vargs = scope.addChannel("...");
+        c_vargs = new ChannelRef.Dynamic<Object>("...", vargs.getIndex());
+        vargs.appendDynamic();
+		super.compileBlocks(w, sig, blocks, scope);
 	}
-	
-	protected Cache getCache(Stack stack, boolean staticdef) throws ExecutionException {
-		Context ctx = this.context.getValue(stack);
-		synchronized(ctx) {
-			Cache c = (Cache) ctx.getAttribute(CACHE);
-			if (c == null) {
-				c = new Cache();
-				c.setMaxCacheSize(-1);
-				ctx.setAttribute(CACHE, c);
-			}
-			return c;
-		}
-	}
+
+	@Override
+    protected void runBody(LWThread thr) {
+        int i = thr.checkSliceAndPopState(2);
+        int frame = thr.popIntState();
+        Object key = thr.popState();
+        Stack stack = thr.getStack();
+        try {
+            switch (i) {
+                case 0:
+                    frame = stack.frameCount();
+                    key = this.on.getValue(stack);
+                    i++;
+                case 1:
+                    boolean shouldContinue = checkCached(stack, key);
+                    if (!shouldContinue) {
+                        break;
+                    }
+                    c_vargs.create(stack);
+                    i++;
+                default:
+                    if (CompilerSettings.PERFORMANCE_COUNTERS) {
+                        startCount++;
+                    }
+                    body.run(thr);
+                    setValue(key, stack);
+            }
+        }
+        catch (Yield y) {
+            y.getState().push(key);
+            y.getState().push(frame);
+            y.getState().push(i, 2);
+            throw y;
+        }
+        catch (ExecutionException e) {
+            stack.dropToFrame(frame);
+            setException(key, thr.getStack(), e);
+            throw e;
+        }
+    }
+
+    protected boolean checkCached(Stack stack, Object key) {
+        return initialize(key, stack);
+    }
+
+    protected boolean initialize(Object key, Stack stack) {
+        FutureObject fv;
+        boolean initial = false;
+        synchronized (cache) {
+            fv = (FutureObject) cache.getCachedValue(key);
+            if (fv == null) {
+                fv = new FutureObject();
+                cache.addAndLock(key, fv);
+                initial = true;
+            }
+        }
+        if (initial) {
+            return true;
+        }
+        else {
+            Returns r = (Returns) fv.getValue();
+            returnCachedValues(stack, r);
+            return false;
+        }
+    }
+
+    public void setValue(Object key, Stack stack) throws ExecutionException {
+        Returns ret = getReturnValues(stack);
+        returnCachedValues(stack, ret);
+        synchronized (cache) {
+            FutureObject f = (FutureObject) cache.getCachedValue(key);
+            f.setValue(ret);
+            cache.unlock(key);
+        }
+    }
+
+    public void setException(Object key, Stack stack, RuntimeException e) {
+        synchronized (cache) {
+            FutureObject f = (FutureObject) cache.getCachedValue(key);
+            f.fail(e);
+        }
+    }
+
+    private Returns getReturnValues(Stack stack) {
+        Returns r = new Returns();
+        r.args.addAll(c_vargs.get(stack).getAll());
+        return r;
+    }
+
+    private void returnCachedValues(Stack stack, Returns ret) {
+        cr_vargs.get(stack).addAll(ret.args);
+    }
+    
+    private static class Returns {
+        public final List<Object> args;
+        
+        public Returns() {
+            args = new LinkedList<Object>();
+        }
+    }
 }
