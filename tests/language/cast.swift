1030d92982ffaf29deb1bd502f78215bbf1eba3f
Max Hutchinson <maxhutch@gmail.com>
2015-11-11 16:47:11 -0600

    Adding another cast, trigger travis.

--- tests/language/cast.swift
+++ tests/language/cast.swift
@@ -1,4 +1,14 @@
+int end_time_int = 4;
+float end_time = toFloat(end_time_int);
+tracef("%i == %f\n", end_time_int, end_time);
+
 float io_time = .25;
-float end_time = 4.0;
+
+
+
 int num_out = toInt(end_time / io_time);
+
+
 tracef("%f / %f = %i\n", end_time, io_time, num_out);
+
+
